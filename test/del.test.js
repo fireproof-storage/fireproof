425256c3ae52845f4ecb49e38b323504c7c5ee5c
Alan Shaw <alan.shaw@protocol.ai>
2022-12-28 22:12:24 +0000

    test: add delete test

---
+++ test/del.test.js
@@ -0,0 +1,27 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert'
+import { ShardBlock, put, get, del } from '../index.js'
+import { Blockstore, randomCID } from './helpers.js'
+
+describe('del', () => {
+  it('deletes a value', async () => {
+    const emptyShard = await ShardBlock.create()
+    const blocks = new Blockstore()
+    await blocks.put(emptyShard.cid, emptyShard.bytes)
+
+    const dataCID = await randomCID(32)
+
+    const { root: root0, additions: additions0 } = await put(blocks, emptyShard.cid, 'test', dataCID)
+    for (const b of additions0) {
+      await blocks.put(b.cid, b.bytes)
+    }
+
+    const { root: root1, additions: additions1 } = await del(blocks, root0, 'test')
+    for (const b of additions1) {
+      await blocks.put(b.cid, b.bytes)
+    }
+
+    const res = await get(blocks, root1, 'test')
+    assert.strictEqual(res, undefined)
+  })
+})
