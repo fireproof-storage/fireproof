6f63c9b7654282dee6b51570311369d88c9dbbd7
Chris Anderson <jchris@gmail.com>
2023-02-22 12:21:01 -0800

    refactor clock

--- clock.js
+++ clock.js
@@ -190,3 +190,153 @@ export async function * vis (blocks, head, options = {}) {
   }
   yield '}'
 }
+
+/**
+ * @template T
+ * @param {import('./block').BlockFetcher} blocks Block storage.
+ * @param {EventLink<T>[]} head
+ * @param {object} [options]
+ * @param {(b: EventBlockView<T>) => string} [options.renderNodeLabel]
+ */
+export async function * since (blocks, head, lastSeen) {
+  const events = new EventFetcher(blocks)
+  const hevents = await Promise.all(head.map(link => events.get(link)))
+  const links = []
+  const nodes = new Set()
+  for (const e of hevents) {
+    nodes.add(e.cid.toString())
+    yield e
+    for (const p of e.value.parents) {
+      yield p
+    }
+    links.push(...e.value.parents)
+  }
+  while (links.length) {
+    const link = links.shift()
+    if (!link) break
+    if (nodes.has(link.toString())) continue
+    nodes.add(link.toString())
+    const block = await events.get(link)
+    yield block
+    for (const p of block.value.parents) {
+      yield p
+    }
+    links.push(...block.value.parents)
+  }
+}
+
+/**
+ * Find the common ancestor event of the passed children. A common ancestor is
+ * the first single event in the DAG that _all_ paths from children lead to.
+ *
+ * @param {import('./clock').EventFetcher} events
+ * @param  {import('./clock').EventLink<EventData>[]} children
+ */
+export async function findCommonAncestor (events, children) {
+  if (!children.length) return
+  const candidates = children.map((c) => [c])
+  while (true) {
+    let changed = false
+    for (const c of candidates) {
+      const candidate = await findAncestorCandidate(events, c[c.length - 1])
+      if (!candidate) continue
+      changed = true
+      c.push(candidate)
+      const ancestor = findCommonString(candidates)
+      if (ancestor) return ancestor
+    }
+    if (!changed) return
+  }
+}
+
+/**
+ * @param {import('./clock').EventFetcher} events
+ * @param {import('./clock').EventLink<EventData>} root
+ */
+async function findAncestorCandidate (events, root) {
+  const { value: event } = await events.get(root)
+  if (!event.parents.length) return root
+  return event.parents.length === 1
+    ? event.parents[0]
+    : findCommonAncestor(events, event.parents)
+}
+
+/**
+ * @template {{ toString: () => string }} T
+ * @param  {Array<T[]>} arrays
+ */
+function findCommonString (arrays) {
+  arrays = arrays.map((a) => [...a])
+  for (const arr of arrays) {
+    for (const item of arr) {
+      let matched = true
+      for (const other of arrays) {
+        if (arr === other) continue
+        matched = other.some((i) => String(i) === String(item))
+        if (!matched) break
+      }
+      if (matched) return item
+    }
+  }
+}
+
+/**
+ * Find and sort events between the head(s) and the tail.
+ * @param {import('./clock').EventFetcher} events
+ * @param {import('./clock').EventLink<EventData>[]} head
+ * @param {import('./clock').EventLink<EventData>} tail
+ */
+export async function findSortedEvents (events, head, tail) {
+  // get weighted events - heavier events happened first
+  /** @type {Map<string, { event: import('./clock').EventBlockView<EventData>, weight: number }>} */
+  const weights = new Map()
+  const all = await Promise.all(head.map((h) => findEvents(events, h, tail)))
+  for (const arr of all) {
+    for (const { event, depth } of arr) {
+      const info = weights.get(event.cid.toString())
+      if (info) {
+        info.weight += depth
+      } else {
+        weights.set(event.cid.toString(), { event, weight: depth })
+      }
+    }
+  }
+
+  // group events into buckets by weight
+  /** @type {Map<number, import('./clock').EventBlockView<EventData>[]>} */
+  const buckets = new Map()
+  for (const { event, weight } of weights.values()) {
+    const bucket = buckets.get(weight)
+    if (bucket) {
+      bucket.push(event)
+    } else {
+      buckets.set(weight, [event])
+    }
+  }
+
+  // sort by weight, and by CID within weight
+  const sorted = Array.from(buckets)
+    .sort((a, b) => b[0] - a[0])
+    .flatMap(([, es]) =>
+      es.sort((a, b) => (String(a.cid) < String(b.cid) ? -1 : 1))
+    )
+  // console.log('sorted', sorted.map(s => s.value.data.value))
+  return sorted
+}
+
+/**
+ * @param {import('./clock').EventFetcher} events
+ * @param {import('./clock').EventLink<EventData>} start
+ * @param {import('./clock').EventLink<EventData>} end
+ * @returns {Promise<Array<{ event: import('./clock').EventBlockView<EventData>, depth: number }>>}
+ */
+async function findEvents (events, start, end, depth = 0) {
+  const event = await events.get(start)
+  const acc = [{ event, depth }]
+  const { parents } = event.value
+  if (parents.length === 1 && String(parents[0]) === String(end)) return acc
+  const rest = await Promise.all(
+    parents.map((p) => findEvents(events, p, end, depth + 1))
+  )
+  return acc.concat(...rest)
+}
