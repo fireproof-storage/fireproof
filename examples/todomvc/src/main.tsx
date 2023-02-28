c6433c88d2773c1037c32fdf7f4339e77fa26051
Chris Anderson <jchris@gmail.com>
2023-02-27 19:44:58 -0800

    better fixtures

--- examples/todomvc/src/main.tsx
+++ examples/todomvc/src/main.tsx
@@ -8,5 +8,5 @@ import 'todomvc-app-css/index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
     <App />
-  // </React.StrictMode >,
+  // </React.StrictMode>
 )
