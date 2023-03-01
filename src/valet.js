5816046aa761a8eded3a0f5161d5efdd2cfc801d
Chris Anderson <jchris@gmail.com>
2023-03-01 10:10:38 -0800

    import indexedb

--- src/valet.js
+++ src/valet.js
@@ -1,5 +1,36 @@
+/* eslint no-undef: "warn" */
 import { CarReader } from '@ipld/car'
 import { CID } from 'multiformats/cid'
+
+const request = indexedDB.open('test', 3)
+request.onupgradeneeded = function () {
+  const db = request.result
+  const store = db.createObjectStore('books', { keyPath: 'isbn' })
+  store.createIndex('by_title', 'title', { unique: true })
+
+  store.put({ title: 'Quarry Memories', author: 'Fred', isbn: 123456 })
+  store.put({ title: 'Water Buffaloes', author: 'Fred', isbn: 234567 })
+  store.put({ title: 'Bedrock Nights', author: 'Barney', isbn: 345678 })
+}
+request.onsuccess = function (event) {
+  const db = event.target.result
+
+  const tx = db.transaction('books')
+
+  tx.objectStore('books').index('by_title').get('Quarry Memories').addEventListener('success', function (event) {
+    console.log('From index:', event.target.result)
+  })
+  tx.objectStore('books').openCursor(IDBKeyRange.lowerBound(200000)).onsuccess = function (event) {
+    const cursor = event.target.result
+    if (cursor) {
+      console.log('From cursor:', cursor.value)
+      cursor.continue()
+    }
+  }
+  tx.oncomplete = function () {
+    console.log('All done!')
+  }
+}
 
 export default class Valet {
   #cars = new Map() // cars by cid
