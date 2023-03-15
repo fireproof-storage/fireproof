866f6d9c3abe10f45c017dafb9dee7d6ee982201
Chris Anderson <jchris@gmail.com>
2023-03-14 19:10:34 -0700

    clear old comments

--- packages/fireproof/src/valet.js
+++ packages/fireproof/src/valet.js
@@ -3,16 +3,7 @@ import { CID } from 'multiformats/cid'
 import { openDB } from 'idb'
 import cargoQueue from 'async/cargoQueue.js'
 
-// const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-// let storageSupported = false
-// try {
-//   storageSupported = window.localStorage && true
-// } catch (e) {}
-// const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
 export default class Valet {
-  #cars = new Map() // cars by cid
-  #cidToCar = new Map() // cid to car
   #db = null
   #uploadQueue = null
   #alreadyEnqueued = new Set()
@@ -59,7 +50,6 @@ export default class Valet {
   }
 
   withDB = async (dbWorkFun) => {
-    // if (!storageSupported) return
     if (!this.#db) {
       this.#db = await openDB('valet', 2, {
         upgrade (db, oldVersion, newVersion, transaction) {
@@ -84,11 +74,6 @@ export default class Valet {
    * @param {*} value
    */
   async parkCar (carCid, value, cids) {
-    // this.#cars.set(carCid, value)
-    // for (const cid of cids) {
-    //   this.#cidToCar.set(cid, carCid)
-    // }
-
     await this.withDB(async (db) => {
       const tx = db.transaction(['cars', 'cidToCar'], 'readwrite')
       await tx.objectStore('cars').put(value, carCid)
@@ -130,45 +115,3 @@ export default class Valet {
     })
   }
 }
-
-// export class MemoryValet {
-//   #cars = new Map() // cars by cid
-//   #cidToCar = new Map() // cid to car
-
-//   /**
-//    *
-//    * @param {string} carCid
-//    * @param {*} value
-//    */
-//   async parkCar (carCid, value, cids) {
-//     this.#cars.set(carCid, value)
-//     for (const cid of cids) {
-//       this.#cidToCar.set(cid, carCid)
-//     }
-//   }
-
-//   async getBlock (dataCID) {
-//     return await this.#valetGet(dataCID)
-//   }
-
-//   /**
-//    * Internal function to load blocks from persistent storage.
-//    * Currently it just searches all the cars for the block, but in the future
-//    * we need to index the block CIDs to the cars, and reference that to find the block.
-//    * This index will also allow us to use accelerator links for the gateway when needed.
-//    * It can itself be a prolly tree...
-//    * @param {string} cid
-//    * @returns {Promise<Uint8Array|undefined>}
-//    */
-//   #valetGet = async (cid) => {
-//     const carCid = this.#cidToCar.get(cid)
-//     if (carCid) {
-//       const carBytes = this.#cars.get(carCid)
-//       const reader = await CarReader.fromBytes(carBytes)
-//       const gotBlock = await reader.get(CID.parse(cid))
-//       if (gotBlock) {
-//         return gotBlock.bytes
-//       }
-//     }
-//   }
-// }
