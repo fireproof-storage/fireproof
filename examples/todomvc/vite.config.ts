d45a1b5300a44238e06f4b9924bcb066a3ae03d7
Chris Anderson <jchris@gmail.com>
2023-04-06 19:41:14 -0700

    logging and build

--- examples/todomvc/vite.config.ts
+++ examples/todomvc/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // nodePolyfills({
+    //   // Whether to polyfill `node:` protocol imports.
+    //   protocolImports: false
+    // })
+  ],
   base: './',
   define: {
     global: {}
