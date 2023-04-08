78d6026493a28d5687f26e26823fa58eb0daf7f9
Chris Anderson <jchris@gmail.com>
2023-04-07 20:32:26 -0700

    name the indexes

--- examples/todomvc/src/App.tsx
+++ examples/todomvc/src/App.tsx
@@ -74,12 +74,12 @@ declare global {
 const defineIndexes = (database) => {
   database.allLists = new Index(database, function (doc, map) {
     if (doc.type === 'list') map(doc.type, doc)
-  })
+  }, null, {name : 'allLists'})
   database.todosByList = new Index(database, function (doc, map) {
     if (doc.type === 'todo' && doc.listId) {
       map([doc.listId, doc.createdAt], doc)
     }
-  })
+  }, null, {name : 'todosByList'})
   window.fireproof = database
   return database
 }
