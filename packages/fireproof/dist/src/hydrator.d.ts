ebc41ee8ee0874dbaf3c55d22f4f913aabf25a1d
Chris Anderson <jchris@gmail.com>
2023-04-10 20:28:53 -0700

    polyfill and ts build

--- packages/fireproof/dist/src/hydrator.d.ts
+++
@@ -1,6 +0,0 @@
-export class Hydrator {
-    static fromJSON(json: any, database: any): any;
-    static snapshot(database: any, clock: any): any;
-    static zoom(database: any, clock: any): Promise<any>;
-}
-//# sourceMappingURL=hydrator.d.ts.map
--- packages/fireproof/dist/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Fireproof } from './fireproof';
-import { DbIndex as Index } from './db-index';
-import { Listener } from './listener';
-import { Hydrator } from './hydrator';
-export { Fireproof, Index, Listener, Hydrator };
-//# sourceMappingURL=index.d.ts.map
