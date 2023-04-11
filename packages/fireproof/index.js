bb4c45270b3d4f9f0c90a8d6e4211c777b440d05
Chris Anderson <jchris@gmail.com>
2023-04-10 20:00:41 -0700

    move to test

--- packages/fireproof/index.js
+++
@@ -1,6 +0,0 @@
-import { Fireproof } from './src/fireproof'
-import { Index } from './src/db-index'
-import { Listener } from './src/listener'
-import { Hydrator } from './src/hydrator'
-
-export { Fireproof, Index, Listener, Hydrator }
---
+++ packages/fireproof/src/index.js
@@ -0,0 +1,6 @@
+import { Fireproof } from './fireproof'
+import { DbIndex as Index } from './db-index'
+import { Listener } from './listener'
+import { Hydrator } from './hydrator'
+
+export { Fireproof, Index, Listener, Hydrator }
