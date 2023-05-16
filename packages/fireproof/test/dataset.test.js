42b28e26f8cd51399b7420adfc06988d40d1e2db
Chris Anderson <jchris@gmail.com>
2023-05-16 08:15:20 -0700

    test for dataset

---
+++ packages/fireproof/test/dataset.test.js
@@ -0,0 +1,23 @@
+import { describe, it, beforeEach } from 'mocha'
+import assert from 'node:assert'
+import { Fireproof } from '../src/fireproof.js'
+
+describe('Create a dataset', () => {
+  let database = Fireproof.storage()
+  beforeEach(async () => {
+    database = Fireproof.storage('name')
+
+    const numDocs = 100
+    for (let i = 0; i < numDocs; i++) {
+      const doc = { _id: `doc${i}`, name: `name${i}`, age: i }
+      const response = await database.put(doc)
+      assert(response)
+      assert(response.id, 'should have id')
+      assert.equal(response.id, doc._id)
+    }
+  })
+  it('gets all docs', async () => {
+    const response = await database.allDocuments()
+    assert.equal(response.rows.length, 100)
+  })
+})
