57bb16fc794ff3d856b764edeebb794974a19362
Chris Anderson <jchris@gmail.com>
2023-05-18 17:23:35 -0700

    loader

--- packages/fireproof/test/dataset.test.js
+++ packages/fireproof/test/dataset.test.js
@@ -1,23 +1,27 @@
 import { describe, it, beforeEach } from 'mocha'
 import assert from 'node:assert'
-import { Fireproof } from '../src/fireproof.js'
+import { Loader } from '../src/loader.js'
+import { join } from 'path'
+import { readFileSync } from 'node:fs'
+
+const TEST_DB_NAME = 'todo-test'
 
 describe('Create a dataset', () => {
-  let database = Fireproof.storage()
+  let db, loader
   beforeEach(async () => {
-    database = Fireproof.storage('name')
-
-    const numDocs = 100
-    for (let i = 0; i < numDocs; i++) {
-      const doc = { _id: `doc${i}`, name: `name${i}`, age: i }
-      const response = await database.put(doc)
-      assert(response)
-      assert(response.id, 'should have id')
-      assert.equal(response.id, doc._id)
-    }
+    loader = new Loader()
+    db = loader.loadDatabase(TEST_DB_NAME)
+    await loader.loadData(db, './test/todos.json')
   })
-  it.skip('gets all docs', async () => {
-    const response = await database.allDocuments()
-    assert.equal(response.rows.length, 100)
+  it('gets all docs', async () => {
+    const response = await db.allDocuments()
+    assert.equal(response.rows.length, 18)
+  }).timeout(10000)
+  it('creates clock file', async () => {
+    const dbPath = join(loader.config.dataDir, TEST_DB_NAME)
+    const clockPath = join(dbPath, 'clock.json')
+    assert.equal(dbPath, '~/.fireproof/todo-test')
+    const clockData = readFileSync(clockPath)
+    assert.equal(clockData, 'clc')
   }).timeout(10000)
 })
