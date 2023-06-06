260518dc2322d53a3deee76682d9d15ca9e55d53
Chris Anderson <jchris@gmail.com>
2023-06-05 18:37:36 -0700

    rollback rollups

--- packages/fireproof/webpack.config.js
+++ packages/fireproof/webpack.config.js
@@ -1,13 +1,8 @@
 import NodePolyfillPlugin from 'node-polyfill-webpack-plugin'
-import { FilerWebpackPlugin } from 'filer/webpack'
-// import nodeExternals from "webpack-node-externals";
 
 export default {
   // Other rules...
-  // target:"web",
-  // externals: [nodeExternals()],
   plugins: [
-    new NodePolyfillPlugin(),
-    new FilerWebpackPlugin()
+    new NodePolyfillPlugin()
   ]
 }
