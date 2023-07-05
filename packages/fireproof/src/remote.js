26422ee2bd1e3b8f8faf92f5a72e08e6d00bccdf
Chris Anderson <jchris@gmail.com>
2023-07-05 14:54:08 -0700

    try w3up

---
+++ packages/fireproof/src/remote.js
@@ -0,0 +1,36 @@
+// when you call database.connect(email)
+// it will return a promise that resolves when the user is logged in
+// and sends you an email
+
+import { create } from '@web3-storage/w3up-client'
+
+export class Remote {
+  client = null
+  name = 'unset'
+  config = {}
+
+  constructor (name, config) {
+    this.name = name
+    this.config = config
+  }
+
+  async connect (email) {
+    try {
+      const client = await create()
+      const gotAuth = await client.authorize(email)
+      console.log('gotAuth', gotAuth)
+      const claims = await client.capability.access.claim()
+      console.log('claims', claims)
+      const space = await client.createSpace('fp.' + this.name)
+      console.log('space', space)
+      const gotSetSpace = await client.setCurrentSpace(space.did())
+      console.log('gotSetSpace', gotSetSpace)
+      const gotReg = await client.registerSpace(email)
+      console.log('gotReg', gotReg)
+      this.client = client
+      console.log('client', client)
+    } catch (err) {
+      console.error('registration failed: ', err)
+    }
+  }
+}
