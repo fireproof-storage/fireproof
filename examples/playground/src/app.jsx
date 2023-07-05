57a9b7e72d540c782fe8125daed4f63e8ec4c959
Chris Anderson <jchris@gmail.com>
2023-07-05 16:01:07 -0700

    build app

--- examples/playground/src/app.jsx
+++ examples/playground/src/app.jsx
@@ -1,13 +1,43 @@
 import ReactDOM from 'react-dom'
 import './style.css'
+// import { Fireproof } from '../../../packages/fireproof/src/fireproof.js'
+// import { useFireproof } from '../../../packages/react/dist/index.mjs'
+import { Fireproof } from '../../../packages/fireproof/dist/src/fireproof.mjs'
+// import { useFireproof } from '../../../packages/react/dist/index.mjs'
+import { useFireproof } from 'use-fireproof'
+// console.log(Fireproof, useFireproof)
+
+let myDatabase = null
+if (!myDatabase) {
+  myDatabase = Fireproof.storage('tomato-park')
+}
 
 const App = () => {
+  console.log('App')
+  const { database, useLiveQuery, useDocument } = useFireproof(myDatabase)
+  console.log('database', database)
+  const items = useLiveQuery('type', { key: 'todo' }).docs
+  const [doc, setDoc, saveDoc] = useDocument({ message: 'new todo', type: 'todo' })
+  console.log('items', items)
   return (
     <>
       <h1>Welcome to Tomato Park</h1>
-      <p>To React esbuild starter</p>
-      <p>"npm run start" for dev server</p>
-      <p>"npm run build" for production build</p>
+      <form>
+        <input value={doc.message} onChange={e => setDoc({ message: e.target.value })} />
+        <button
+          onClick={e => {
+            e.preventDefault()
+            saveDoc()
+          }}
+        >
+          Save
+        </button>
+      </form>
+      <ul>
+        {items.map(item => (
+          <li key={item.key}>{item.message}</li>
+        ))}
+      </ul>
     </>
   )
 }
