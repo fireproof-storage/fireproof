bbf0be4ef2ebf30404593a57cfecb2fb3924c986
Chris Anderson <jchris@gmail.com>
2023-07-05 17:43:00 -0700

    app is clean

--- examples/playground/src/app.jsx
+++ examples/playground/src/app.jsx
@@ -1,28 +1,49 @@
 import ReactDOM from 'react-dom'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 // import { Fireproof } from '../../../packages/fireproof/src/fireproof.js'
 // import { useFireproof } from '../../../packages/react/dist/index.mjs'
-// import { Fireproof } from '../../../packages/fireproof/dist/src/fireproof.mjs'
+import { Fireproof } from '../../../packages/fireproof/dist/src/fireproof.mjs'
 // import { useFireproof } from '../../../packages/react/dist/index.mjs'
-import { useFireproof } from 'use-fireproof'
+// import { useFireproof } from 'use-fireproof'
 // console.log(Fireproof, useFireproof)
 
+const database = Fireproof.storage('tomato-park')
+
+window.fireproof = database
+
 const App = () => {
-  console.log('App')
-  const { database, useLiveQuery, useDocument } = useFireproof()
-  console.log('database', database)
-  const items = useLiveQuery('type', { key: 'todo' }).docs
-  const [doc, setDoc, saveDoc] = useDocument({ message: 'new todo', type: 'todo' })
-  console.log('items', items)
+  //   console.log('App')
+  //   const { database, useLiveQuery, useDocument } = useFireproof()
+//   console.log('database', database)
+
+  const [items, setItems] = useState([])
+
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    const onChange = async () => {
+      const docs = await database.allDocuments()
+      //   console.log('docs', docs)
+      setItems(docs.rows)
+    }
+    onChange()
+    return database.subscribe(onChange)
+  }, [database])
+
+  //   const items = useLiveQuery('type', { key: 'todo' }).docs
+  //   const [doc, setDoc, saveDoc] = useDocument({ message: 'new todo', type: 'todo' })
+  //   console.log('items', items)
   return (
     <>
       <h1>Welcome to Tomato Park</h1>
       <form>
-        <input value={doc.message} onChange={e => setDoc({ message: e.target.value })} />
+        <input value={message} onChange={e => setMessage(e.target.value)} />
         <button
           onClick={e => {
             e.preventDefault()
-            saveDoc()
+            database.put({ message, type: 'todo' })
+            setMessage('')
           }}
         >
           Save
@@ -30,7 +51,7 @@ const App = () => {
       </form>
       <ul>
         {items.map(item => (
-          <li key={item.key}>{item.message}</li>
+          <li key={item.key}>{item.value.message}</li>
         ))}
       </ul>
     </>
