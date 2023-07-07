2776140e759b35bda9a686a01ce4a9835084a310
Chris Anderson <jchris@gmail.com>
2023-07-07 14:36:38 -0700

    clock proof of concept

--- packages/fireproof/src/remote.js
+++ packages/fireproof/src/remote.js
@@ -3,15 +3,35 @@
 // and sends you an email
 
 import { create } from '@web3-storage/w3up-client'
+import * as w3clock from '@web3-storage/clock/client'
+import { CID } from 'multiformats'
 
 export class Remote {
   client = null
   name = 'unset'
   config = {}
 
-  constructor (name, config) {
+  constructor (database, name, config) {
     this.name = name
     this.config = config
+    this.database = database
+  }
+
+  async clock (cid) {
+    // const did = this.client.currentSpace()
+    const agent = this.client.agent()
+    const head = await w3clock.head({ issuer: agent, with: agent.did(), proofs: [] })
+    console.log('head', head, JSON.stringify(head.root.data.ocm.out))
+    const headCids = head.root.data.ocm.out.ok.head
+    const blocks = await Promise.all([this.database.blocks.get(CID.parse(cid)),
+      ...headCids.map(c => this.database.blocks.get(c))])
+
+    console.log('blocks', blocks)
+    const adv = await w3clock.advance({ issuer: agent, with: agent.did(), proofs: [] }, CID.parse(cid)
+      , { blocks }
+    )
+    console.log('adv', adv, JSON.stringify(adv.root.data.ocm.out))
+    return { head, adv }
   }
 
   async connect (email) {
