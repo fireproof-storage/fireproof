a87632f464a27e681570a2dff1effb8f9a3a98b6
Chris Anderson <jchris@gmail.com>
2023-07-08 15:32:06 -0700

    notes

--- packages/fireproof/src/remote.js
+++ packages/fireproof/src/remote.js
@@ -34,6 +34,28 @@ export class Remote {
     return { head, adv }
   }
 
+  async sync (cid) {
+    // fetch the remote clock headCids using w3clock.head
+    // if it is the same as the local (current metadata carcid? `newValetCidCar` / sync clock), do nothing, we are in sync
+    // if it is the same as our previously pushed clock event, but our local clock is ahead of it, we need to push our clock
+    //               - we can store the previous clock event cid in the metadata
+    // - sending our updates:
+    //   - get the _last_sync and _last_compact values from our metadata
+    //   - if last sync is after last compact
+    //   - else
+    //     - upload the car file for the last compact
+    //     - make a merge car file for any uncompacted car files since the last compact, it should base its cidMap on the compact car file (as we go the sync stream will need to track it's own cidMap)
+    //        - if there is only one car file, it is the merge car file (already based on last compact)
+    //     - upload the merge car file
+    //     -  create a new clock block with the current w3clock.head as parent and the merge car file cid as the data
+    //     - update the remote clock with the new clock block (it doesn't need to fetch the car file, and we dont need to store the clock blocks locally, just the most recent one)
+    //
+    // else if the remote head is not contained by our clock, it is is ahead of the local sync clock.
+    // -  get the car file it points to from its data field
+    // - merge to the local clock (park that car so we have both carcid indexes)
+    // - calculate a new root from the merged head, and update the local clock
+  }
+
   async connect (email) {
     try {
       const client = await create()
