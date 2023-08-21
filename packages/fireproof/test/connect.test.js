46f6eae9b58e06fb3d7bf0a267235683c7937d0b
Chris Anderson <jchris@gmail.com>
2023-08-21 13:48:32 -0700

    use mock connect for tests

--- packages/fireproof/test/connect.test.js
+++ packages/fireproof/test/connect.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable mocha/max-top-level-suites */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
@@ -32,13 +33,56 @@ const mockConnect = {
   }
 }
 
-describe('basic Connection with s3 remote', function () {
+describe.skip('basic Connection with s3 remote', function () {
+  /** @type {Database} */
+  let db, dbName
+  beforeEach(async function () {
+    dbName = 'test-s3-' + Math.ceil(Math.random() * 100000)
+    db = new Database(dbName)
+    const remote = connect.s3(db, serviceConfig.s3)
+    await remote.ready
+    /** @type {Doc} */
+    const doc = { _id: 'hello', value: 'world' }
+    const ok = await db.put(doc)
+    equals(ok.id, 'hello')
+  })// .timeout(10000)
+  it('should save a remote header', async function () {
+    const { _crdt: { blocks: { loader } } } = db
+    const gotMain = await loader.remoteMetaStore.load('main')
+    assert(gotMain)
+    equals(gotMain.key, loader.key)
+  }).timeout(10000)
+  it('should get', async function () {
+    const doc = await db.get('hello')
+    assert(doc)
+    equals(doc._id, 'hello')
+    equals(doc.value, 'world')
+  }).timeout(10000)
+  it('should get remote', async function () {
+    await resetDirectory(MetaStore.dataDir, dbName)
+    const db2 = new Database(dbName)
+    const remote = connect.s3(db2, serviceConfig.s3)
+    await remote.ready
+    const { _crdt: { blocks: { loader: loader2 } } } = db2
+    await loader2.ready
+    const gotMain = await loader2.remoteMetaStore.load('main')
+    equals(gotMain.key, loader2.key) // fails when remote not ingested
+
+    const doc = await db2.get('hello')
+
+    assert(doc)
+    equals(doc._id, 'hello')
+    equals(doc.value, 'world')
+  }).timeout(10000)
+})
+
 describe('basic Connection with raw remote', function () {
   /** @type {Database} */
   let db, dbName
   beforeEach(async function () {
     dbName = 'test-s3-' + Math.ceil(Math.random() * 100000)
     db = new Database(dbName)
+    mockStore.clear()
     const remote = connect.raw(db, mockConnect)
     await remote.ready
     /** @type {Doc} */
@@ -82,7 +126,8 @@ describe('forked Connection with s3 remote', function () {
   beforeEach(async function () {
     dbName = 'test-s3-' + Math.ceil(Math.random() * 100000)
     db = new Database(dbName)
-    const remote = connect.s3(db, serviceConfig.s3)
+    mockStore.clear()
+    const remote = connect.raw(db, mockConnect)
     await remote.ready
     /** @type {Doc} */
     const doc = { _id: 'hello', value: 'world' }
@@ -106,7 +151,7 @@ describe('forked Connection with s3 remote', function () {
     const db2 = new Database(dbName)
     const ok2 = await db2.put({ _id: 'greetings', value: 'universe' })
     equals(ok2.id, 'greetings')
-    const remote = connect.s3(db2, serviceConfig.s3)
+    const remote = connect.raw(db2, mockConnect)
     await remote.ready
     const { _crdt: { blocks: { loader: loader2 } } } = db2
     const gotMain = await loader2.remoteMetaStore.load('main')
