d387872f9f9c87834cc1872d129d0495fd6e13bb
Chris Anderson <jchris@gmail.com>
2023-08-28 14:28:46 -0700

    first pass impl

--- packages/fireproof/src/connect-web3.ts
+++ packages/fireproof/src/connect-web3.ts
@@ -1,31 +1,35 @@
-/* eslint-disable import/first */
-// console.log('import store-s3')
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { create, Client } from '@web3-storage/w3up-client'
 
 import { Connection, DownloadFnParams, UploadFnParams } from './types'
 import { validateParams } from './connect'
 
 export class ConnectWeb3 implements Connection {
-  email: string
-  ready: Promise<any>
+  email: `${string}@${string}`
+  ready: Promise<void>
+  client: Client | null = null
 
-  constructor(email: string) {
+  constructor(email: `${string}@${string}`) {
     this.email = email
     this.ready = this.initializeClient()
   }
 
   async initializeClient() {
-
+    this.client = await getClient(this.email)
   }
 
   async upload(bytes: Uint8Array, params: UploadFnParams) {
+    console.log('awaiting upload', this.client)
+    await this.ready
     validateParams(params)
-    // const fetchUploadUrl = new URL(`${this.uploadUrl.toString()}?${new URLSearchParams(params).toString()}`)
-    // const response = await fetch(fetchUploadUrl)
-    // const { uploadURL } = await response.json() as { uploadURL: string }
-    // await fetch(uploadURL, { method: 'PUT', body: bytes })
+    console.log('uploading', params)
+    await this.client?.uploadFile(new Blob([bytes]))
   }
 
   async download(params: DownloadFnParams) {
+    await this.ready
     validateParams(params)
     const { type, name, car, branch } = params
     // const fetchFromUrl = new URL(`${type}/${name}/${type === 'meta'
@@ -36,4 +40,16 @@ export class ConnectWeb3 implements Connection {
     // return bytes
     return new Uint8Array()
   }
+}
+
+export async function getClient(email: `${string}@${string}`) {
+  const client = await create()
+  await client.authorize(email)
+  let space = client.currentSpace()
+  if (space === undefined) {
+    space = await client.createSpace()
+    await client.setCurrentSpace(space.did())
+    await client.registerSpace(email)
+  }
+  return client
 }
