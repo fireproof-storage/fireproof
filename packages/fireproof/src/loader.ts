cca4b5f00566c16dae8e6a959ff962467114cbad
Chris Anderson <jchris@gmail.com>
2023-08-29 08:51:15 -0700

    s3 test passing

--- packages/fireproof/src/loader.ts
+++ packages/fireproof/src/loader.ts
@@ -186,13 +186,16 @@ export abstract class Loader {
     }
 
     await this.carStore!.save({ cid, bytes })
-    this.remoteCarStore?.save({ cid, bytes }).then(async () => {
-      await this.remoteMetaStore?.load('main').then(async (meta) => {
-        await this.remoteMetaStore?.save({ car: cid, key: theKey || null })
-        if (meta) {
-          await this.mergeMetaFromRemote(meta)
-        }
-      })
+    console.log('saving remote car', cid.toString())
+    this.remoteMetaLoading = this.remoteCarStore?.save({ cid, bytes }).then(async () => {
+      console.log('saving remote meta', cid.toString())
+
+      // await this.remoteMetaStore?.load('main').then(async (meta) => {
+      await this.remoteMetaStore?.save({ car: cid, key: theKey || null })
+      // if (meta) {
+      // await this.mergeMetaFromRemote(meta)
+      // }
+      // })
     }).catch((e) => {
       console.error('Failed to save remote car or meta', e)
     })
