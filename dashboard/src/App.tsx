077f3d34864069cb72d3a688dceb200aba7777cb
Chris Anderson <jchris@gmail.com>
2023-09-01 11:04:40 -0700

    layout

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,16 +1,26 @@
 import './App.css'
 
 import { Route, Routes } from 'react-router-dom'
+import { Header } from './components/Header'
 
 function App() {
   return (
-    <div className="App bg-blue-500 text-white">
-      <Routes>
-      <Route path="/about" element={<div>About</div>} />
-        <Route path="/contact" element={<div>Contact</div>} />
-        <Route path="/" element={<div>Home</div>} />
-      </Routes>
-    </div>
+    <>
+      <Header />
+      <div className="App flex min-h-screen">
+        <div className="Sidebar w-1/4 dark:bg-gray-700 bg-slate-400 p-4">
+          {/* Sidebar content */}
+          <div>Sidebar</div>
+        </div>
+        <div className="MainContent flex-1 p-4">
+          <Routes>
+            <Route path="/about" element={<div>About</div>} />
+            <Route path="/contact" element={<div>Contact</div>} />
+            <Route path="/" element={<div>Home</div>} />
+          </Routes>
+        </div>
+      </div>
+    </>
   )
 }
 
