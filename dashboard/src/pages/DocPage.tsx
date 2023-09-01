231e58b3aa739d379ea3b48901748ca656cab0f8
Chris Anderson <jchris@gmail.com>
2023-09-01 14:54:41 -0700

    saving works

--- dashboard/src/pages/DocPage.tsx
+++ dashboard/src/pages/DocPage.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import { useFireproof } from 'use-fireproof'
 import { CodeHighlight, EditableCodeHighlight } from '../components/CodeHighlight'
 import { Doc, DocFragment } from '@fireproof/core'
+import { useState } from 'react'
 
 export function DocPage() {
   const { dbName, docId } = useParams()
@@ -9,6 +10,7 @@ export function DocPage() {
   const { useDocument } = useFireproof(dbName as string)
 
   const [doc, setDoc, saveDoc] = useDocument({ _id: docId as string })
+  const [needsSave, setNeedsSave] = useState(false)
 
   console.log(dbName, docId, doc)
   const { data, meta } = dataAndMeta(doc)
@@ -16,15 +18,18 @@ export function DocPage() {
   const title = meta._id ? `Edit: ${meta._id}` : 'Create new document'
 
   function editorChanged({ code, valid }: { code: string; valid: boolean }) {
-    // setNeedsSave(valid)
+    setNeedsSave(valid)
+    if (valid)
+    setDoc(JSON.parse(code))
     // setDocToSave(code)
-    console.log(code, valid)
+    console.log({code, valid})
   }
 
   return (
     <div className="flex flex-col">
       <h1 className="text-2xl font-bold">{title}</h1>
       <EditableCodeHighlight code={JSON.stringify(data, null, 2)} onChange={editorChanged} />
+      <button title="Save" disabled={!needsSave} onClick={() => saveDoc()} className="btn btn-primary">Save</button>
       <CodeHighlight code={JSON.stringify(meta, null, 2)} />
     </div>
   )
