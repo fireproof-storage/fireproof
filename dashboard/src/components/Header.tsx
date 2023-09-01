8001c268e16bc1286ece74face45df43ba3cd759
Chris Anderson <jchris@gmail.com>
2023-09-01 11:04:40 -0700

    layout

---
+++ dashboard/src/components/Header.tsx
@@ -0,0 +1,26 @@
+export function Header() {
+  return (
+    <header className="bg-gray-800 pt-3 pb-1 px-5 flex items-center justify-between">
+      <a href="https://fireproof.storage/">
+        <img
+          src="https://fireproof.storage/static/img/logo-animated.svg"
+          alt="Fireproof Logo"
+          className="logo"
+        />
+      </a>
+      <nav className="nav-menu text-white">
+        <ul className="flex">
+          <li className="mr-6">
+            <a href="https://fireproof.storage/developer/">Docs</a>
+          </li>
+          <li className="mr-6">
+            <a href="https://fireproof.storage/blog/">Blog</a>
+          </li>
+          <li className="mr-6">
+            <a href="https://fireproof.storage/thanks/">Community</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
