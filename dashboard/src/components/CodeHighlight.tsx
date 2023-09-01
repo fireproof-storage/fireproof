e1c091d2b3fd7f7f3a3b8b1690288028873b3336
Chris Anderson <jchris@gmail.com>
2023-09-01 14:09:15 -0700

    doc editor plumbed

--- dashboard/src/components/CodeHighlight.tsx
+++ dashboard/src/components/CodeHighlight.tsx
@@ -3,14 +3,14 @@ import { useRef, useState, useCallback, useEffect } from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import { useEditable } from 'use-editable'
 
-export function CodeHighlight({ code, theme, language = 'json' }: any) {
+export function CodeHighlight({ code, theme = defaultProps.theme, language = 'json' }: any) {
   // const editorRef = useRef(null)
 
   return (
     <div className="p-2">
       <Highlight
         {...defaultProps}
-        theme={theme || defaultProps.theme}
+        theme={theme}
         code={code}
         language={language}
       >
@@ -32,7 +32,7 @@ export function CodeHighlight({ code, theme, language = 'json' }: any) {
   )
 }
 
-export function EditableCodeHighlight({ code, theme, onChange, language = 'json' }: any) {
+export function EditableCodeHighlight({ code, onChange, theme = defaultProps.theme, language = 'json' }: any) {
   const editorRef = useRef(null)
   const [liveCode, setCode] = useState(code)
   // console.log('liveCode', liveCode, code)
@@ -65,7 +65,7 @@ export function EditableCodeHighlight({ code, theme, onChange, language = 'json'
     <div className="p-2">
       <Highlight
         {...defaultProps}
-        theme={theme || defaultProps.theme}
+        theme={theme}
         code={liveCode}
         language={language}
       >
