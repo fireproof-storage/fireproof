298680f805dbd689b067c71824158fdf8b922053
Chris Anderson <jchris@gmail.com>
2023-09-01 11:44:55 -0700

    outline

---
+++ dashboard/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-center">
+        Welcome to Fireproof!
+      </h1>
+      <p className="text-xl text-center">
+        This is the home page.
+      </p>
+    </div>
+  )
+}
