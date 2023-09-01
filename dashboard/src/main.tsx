6b0e0f27e53216b138bc9fa4bc65c1d487cd11e9
Chris Anderson <jchris@gmail.com>
2023-09-01 10:33:29 -0700

    tailwind

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
+import './tailwind.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'media', 
+  content: [
+    './src/**/*.tsx', // TypeScript JSX files
+    './src/**/*.ts',  // TypeScript files
+    './index.html' // HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
