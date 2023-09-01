fe3447a6e2c25fdd811d1ea71003f6d99406f9b2
Chris Anderson <jchris@gmail.com>
2023-09-01 11:23:19 -0700

    layout and header, dark mode

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -8,11 +8,11 @@ function App() {
     <>
       <Header />
       <div className="App flex min-h-screen">
-        <div className="Sidebar w-1/4 dark:bg-gray-700 bg-slate-400 p-4">
+        <div className="Sidebar w-1/4  p-4">
           {/* Sidebar content */}
           <div>Sidebar</div>
         </div>
-        <div className="MainContent flex-1 p-4">
+        <div className="MainContent flex-1 p-4 dark:bg-gray-700 bg-slate-200">
           <Routes>
             <Route path="/about" element={<div>About</div>} />
             <Route path="/contact" element={<div>Contact</div>} />
