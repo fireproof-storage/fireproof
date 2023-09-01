0e3231e465948a7e568393c81ef680c3e3ab0a62
Chris Anderson <jchris@gmail.com>
2023-09-01 12:23:29 -0700

    links

--- dashboard/src/components/Sidebar.tsx
+++ dashboard/src/components/Sidebar.tsx
@@ -1,10 +1,21 @@
-import { useParams } from "react-router-dom"
+import { useParams } from 'react-router-dom'
 
 export function Sidebar() {
-  const {dbName} = useParams()
-  console.log(dbName)
-  const title = dbName ? `Sidebar / ${dbName}` : "Sidebar"
-  return (<div className="Sidebar w-1/4  p-4">
-    <div>{title}</div>
-  </div>)
+  const { dbName } = useParams()
+  const title = dbName ? `Database` : 'Databases'
+  return (
+    <div className="Sidebar w-1/4  p-4">
+      <ul className="mt-4">
+        <li className="mb-2">
+          <a href="/databases">{title}</a>
+          {dbName && (
+            <>
+              {' '}
+              / <a href={'/db/' + dbName}><code>{dbName}</code></a>
+            </>
+          )}
+        </li>
+      </ul>
+    </div>
+  )
 }
