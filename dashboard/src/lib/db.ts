f74483e246d819f8f5bcbd662684d0f923570f30
Chris Anderson <jchris@gmail.com>
2023-09-02 14:05:10 -0700

    restore snapshots

---
+++ dashboard/src/lib/db.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { CID } from 'multiformats'
+import { fireproof, Database, DocFragment } from 'use-fireproof'
+export async function snapshot(db: Database, name: string) {
+  const snap = fireproof(name)
+  await snap._crdt.ready
+  const snapshot = await snap._crdt.blocks.loader?.metaStore?.load()
+  if (snapshot) {
+    console.log('snapshot', snapshot)
+    await db.put({
+      type: 'snapshot',
+      created: Date.now(),
+      name: name,
+      // @ts-ignore
+      snapshot
+    })
+  }
+}
+
+export async function ensureNamed(db: Database, name: string) {
+  await db.get('db:' + name).catch(() => db.put({ _id:'db:' + name, type: 'db', added: Date.now() }))
+}
+
+
+export async function restore(dbName: string, {key, car}: {key: string, car: string}) {
+  const snap = fireproof(dbName)
+  console.log('restoring', key, car)
+  await snap._crdt.blocks.loader?.metaStore?.save({ key, car: CID.parse(car.toString()) })
+}
--- dashboard/src/pages/Database.tsx
+++ dashboard/src/pages/Database.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useParams } from 'react-router-dom'
 // import { headersForDocs } from '../components/dynamicTableHelpers'
 // import DynamicTable from '../components/DynamicTable'
 import { Doc, ChangesResponse } from '@fireproof/core'
-import { useFireproof } from 'use-fireproof'
+import { fireproof, useFireproof } from 'use-fireproof'
+import { CID } from 'multiformats'
+import { restore, snapshot } from '../lib/db'
 
 export function Database() {
   const { dbName } = useParams()
-  const { useLiveQuery } = useFireproof('_dashboard')
+  const { database: dashDb, useLiveQuery } = useFireproof('_dashboard')
   // const {database, useLiveQuery : ulq} = useFireproof(dbName as string)
 
   const snapshots = useLiveQuery((doc, map) => {
@@ -14,23 +17,30 @@ export function Database() {
       map(doc.name!)
     }
   })
-  // const snapshots = useLiveQuery('_id')
+
+  const doRestore =
+    ({ snapshot: { key, car } }: { snapshot: { key: string; car: string } }) =>
+    async () => {
+      console.log('restoring', key, car)
+      await snapshot(dashDb, dbName!)
+
+      await restore(dbName!, { key, car })
+    }
 
   console.log(snapshots)
-  const result = snapshots
   return (
     <div className="flex flex-col">
       <h2 className="text-2xl">{dbName}</h2>
       <p>This database is has additional metadata tracked by the dashboard.</p>
       <h3 className="text-xl mt-2">Snapshots</h3>
       <ul>
-        {snapshots.docs.map((doc: Doc) => (
+        {snapshots.docs.map((doc: any) => (
           <li key={doc._id}>
             <span>
               Database: <strong>{doc.name}</strong> Snapped at:{' '}
               {new Date(doc.created).toLocaleString()}
             </span>
-            <button>Restore</button>
+            <button onClick={doRestore(doc)}>Restore</button>
           </li>
         ))}
       </ul>
