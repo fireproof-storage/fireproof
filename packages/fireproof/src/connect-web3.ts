6e0a6ec4bf5a9f23f787b009366a6d7410310807
Chris Anderson <jchris@gmail.com>
2023-09-06 17:51:05 -0700

    format authorize

--- packages/fireproof/src/connect-web3.ts
+++ packages/fireproof/src/connect-web3.ts
@@ -142,7 +142,11 @@ export async function getClient(email: `${string}@${string}`) {
     }
   }
   console.log('emailing!!', email, client, client.spaces())
-  await client.authorize(email, { capabilities: [{ can: 'clock/*' }, { can: 'space/*' }, { can: 'provider/add' }, { can: 'store/*' }, { can: 'upload/*' }] })
+  await client.authorize(email, {
+    capabilities: [{ can: 'clock/*' },
+      { can: 'space/*' }, { can: 'provider/add' },
+      { can: 'store/*' }, { can: 'upload/*' }]
+  })
   // await client.capability.access.claim()
   console.log('authorized??', client)
   let space = client.currentSpace()
