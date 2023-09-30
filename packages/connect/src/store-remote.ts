3fb8bf11443d6089caff639e2e8f5b73ca6fecf1
Chris Anderson <jchris@gmail.com>
2023-09-30 07:33:25 -0700

    replace atob

--- packages/connect/src/store-remote.ts
+++ packages/connect/src/store-remote.ts
@@ -75,18 +75,23 @@ export class RemoteMetaStore extends MetaStoreBase {
     return `fp.${this.name}.${this.STORAGE_VERSION}`
   }
 
+  async handleByteHeads(byteHeads: Uint8Array[], branch: string = 'main') {
+    const dbMetas = this.dbMetasForByteHeads(byteHeads)
+    // console.log('dbMetasForByteHeads', dbMetas.map((m) => m.car.toString()))
+    const subscribers = this.subscribers.get(branch) || []
+    for (const subscriber of subscribers) {
+      await subscriber(dbMetas)
+    }
+    return dbMetas
+  }
+
   async load(branch: string = 'main'): Promise<DbMeta[] | null> {
     const byteHeads = await this.connection.metaDownload({
       name: this.prefix(),
       branch
     })
     if (!byteHeads) return null
-    const dbMetas = this.dbMetasForByteHeads(byteHeads)
-    const subscribers = this.subscribers.get(branch) || []
-    for (const subscriber of subscribers) {
-      await subscriber(dbMetas)
-    }
-    return dbMetas
+    return this.handleByteHeads(byteHeads, branch)
   }
 
   async save(meta: DbMeta, branch: string = 'main') {
