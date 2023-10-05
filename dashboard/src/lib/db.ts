e7a836f3d9a47f70c01815ea483f22a62ddbae05
Chris Anderson <jchris@gmail.com>
2023-10-05 07:43:27 -0700

    move to 0.14

--- dashboard/src/lib/db.ts
+++ dashboard/src/lib/db.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { CID } from 'multiformats'
-import { fireproof, connect, Database } from 'use-fireproof'
+import { fireproof, Database } from 'use-fireproof'
+import { connect } from '@fireproof/connect'
 export async function snapshot(db: Database, name: string) {
   const snap = fireproof(name)
   await snap._crdt.ready
   const snapshot = await snap._crdt.blocks.loader?.metaStore?.load()
-  if (snapshot) {
+  if (snapshot) { // .length === 1
     const snaps = await db.query((doc, emit) => { if (doc.type === 'snapshot') emit(doc.name) }, { key: name })
     // @ts-ignore
     if (snaps.rows.some((s) => s.doc.snapshot!.car.toString() === snapshot.car.toString())) {
@@ -20,6 +21,7 @@ export async function snapshot(db: Database, name: string) {
     })
   }
 }
+// https://sam-app-s3uploadbucket-e6rv1dj2kydh.s3.us-east-2.amazonaws.com/data/fp.fp.thrus1/bafkreiejpc3kl2wasu4nyeozgubulohiqifyt3vu2vcoauzfrnxwc2rrrq.car
 
 export async function ensureNamed(db: Database, name: string) {
   await db.get('db:' + name).catch(() => db.put({ _id: 'db:' + name, type: 'db', added: Date.now() }))
@@ -30,22 +32,48 @@ export async function restore(dbName: string, { key, car }: { key: string, car: 
   await snap._crdt.blocks.loader?.metaStore?.save({ key, car: CID.parse(car.toString()) })
 }
 
+const fetchCarIPFS = async ({ car }: { car: string }) => {
+  const url = `https://${car}.ipfs.w3s.link/`
+  const response = await fetch(url)
+  if (response.ok) {
+    return new Uint8Array(await response.arrayBuffer())
+  } else {
+    // console.log('failed to download', url, response)
+    throw new Error(`Failed to download ${url}`)
+  }
+}
+
+const fetchCarS3 = async ({ car, name }: { car: string, name:string }) => {
+  const host = `https://sam-app-s3uploadbucket-e6rv1dj2kydh.s3.us-east-2.amazonaws.com`
+  const url = `${host}/data/${name}/${car}.car`
+  const response = await fetch(url)
+  if (response.ok) {
+    return new Uint8Array(await response.arrayBuffer())
+  } else {
+    // console.log('failed to download', url, response)
+    throw new Error(`Failed to download ${url}`)
+  }
+}
+
 export function inspectDb(dbName: string) {
   if (!dbName) throw new Error('no dbName')
-  if (dbName === '_dashboard') throw new Error('are you sure you want to inspect the dashboard?')
+  if (dbName === '_dashboard') throw new Error('are you sure you want to inspect the dashboard database?')
   const db = fireproof(dbName)
+
   // @ts-ignore
   connect.raw(db, {
-    upload: () => false, download: async ({ type, car }) => {
-      if (type === 'meta') { return false }
-      const url = `https://${car}.ipfs.w3s.link/`
-      const response = await fetch(url)
-      if (response.ok) {
-        return new Uint8Array(await response.arrayBuffer())
-      } else {
-        console.log('failed to download', url, response)
-        throw new Error(`Failed to download ${url}`)
-      }
+    dataUpload: () => false, 
+    metaUpload: () => false, 
+    metaDownload: () => false, 
+    dataDownload: async (params) => {
+      const got = await Promise.any([
+        fetchCarIPFS(params),
+        fetchCarS3(params)
+      ]).catch((e) => {
+        console.log('failed to download', e)
+        throw new Error(`Failed to download ${params.car}`)
+      })
+      return got
     }
   })
   return db
