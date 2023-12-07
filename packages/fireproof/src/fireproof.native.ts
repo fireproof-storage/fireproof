cb69a925dc8a7fccec73858648558d99efde35fa
Brad Anderson <brad@sankatygroup.com>
2023-12-07 18:38:56 -0500

    upgrade to RN 0.73

---
+++ packages/fireproof/src/fireproof.native.ts
@@ -0,0 +1,10 @@
+// ts-unused-exports:disable-next-line
+export * from './database'
+// ts-unused-exports:disable-next-line
+export * from './index'
+// ts-unused-exports:disable-next-line
+export * from './loader-helpers'
+
+export const FIREPROOF_PLATFORM='native';
+export * from './store'
+export * from './loader'
