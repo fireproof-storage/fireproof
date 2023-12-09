7a8b89b9d26ca1ee327cd6ce3abdf60e9e0c7721
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2023-12-09 21:24:38 +0000

    making storage available inside partykit

--- packages/connect-partykit/src/index.ts
+++ packages/connect-partykit/src/index.ts
@@ -17,6 +17,26 @@ export const connect = {
     if (!refresh && partyCxs.has(name)) {
       return partyCxs.get(name)!
     }
+    if (!partyHost) {
+      console.warn('partyHost not provided, using localhost:1999')
+      partyHost = 'http://localhost:1999'
+    }
+    const connection = new ConnectPartyKit({ name, host: partyHost } as ConnectPartyKitParams)
+    connection.connect(loader!)
+    partyCxs.set(name, connection)
+    return connection
+  },
+  partykitS3: (db: Database, partyHost?: string, refresh?: boolean) => {
+    const {
+      name,
+      _crdt: {
+        blocks: { loader }
+      }
+    } = db
+    if (!name) throw new Error('database name is required')
+    if (!refresh && partyCxs.has(name)) {
+      return partyCxs.get(name)!
+    }
     const s3conf = {
       upload: 'https://04rvvth2b4.execute-api.us-east-2.amazonaws.com/uploads',
       download: 'https://sam-app-s3uploadbucket-e6rv1dj2kydh.s3.us-east-2.amazonaws.com'
