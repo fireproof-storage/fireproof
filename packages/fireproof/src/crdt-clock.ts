c7d999bc516dc0f5224e9f6d2f905bd4e748e29e
Chris Anderson <jchris@gmail.com>
2023-12-19 14:29:35 -0800

    clock work

--- packages/fireproof/src/crdt-clock.ts
+++ packages/fireproof/src/crdt-clock.ts
@@ -1,5 +1,4 @@
 import { clockChangesSince } from './crdt-helpers'
-// import { uniqueCids } from './loader'
 import { TransactionBlockstore, Transaction } from './transaction'
 import type { DocUpdate, BulkResult, ClockHead } from './types'
 import { advance } from '@alanshaw/pail/clock'
@@ -7,7 +6,7 @@ import { root } from '@alanshaw/pail/crdt'
 import { applyHeadQueue, ApplyHeadQueue } from './apply-head-queue'
 
 export class CRDTClock {
-  // todo: keep the clock of remote and local changes separate, merge on read
+  // todo: track local and remote clocks independently, merge on read
   // that way we can drop the whole remote if we need to
   // should go with making sure the local clock only references locally available blocks on write
   head: ClockHead = []
@@ -28,17 +27,8 @@ export class CRDTClock {
     this.head = head
   }
 
-  async applyHead(
-    // tblocks: Transaction | null,
-    newHead: ClockHead,
-    prevHead: ClockHead,
-    updates: DocUpdate[] | null = null
-  ) {
-    const taskId = Math.random().toString().slice(2, 8)
-    // console.log('applyHead', taskId, updates?.length, 'og', this.head.sort((a, b) => a.toString().localeCompare(b.toString())).toString(), 'new', newHead.toString(), 'prev', prevHead.toString())
+  async applyHead(newHead: ClockHead, prevHead: ClockHead, updates: DocUpdate[] | null = null) {
     for await (const { updates: updatesAcc, all } of this.applyHeadQueue.push({
-      id: taskId,
-      // tblocks,
       newHead,
       prevHead,
       updates
@@ -57,13 +47,7 @@ export class CRDTClock {
     }
   }
 
-  async int_applyHead(
-    taskId: string,
-    // tblocks: Transaction | null,
-    newHead: ClockHead,
-    prevHead: ClockHead
-    // updates: DocUpdate[] | null = null
-  ) {
+  async int_applyHead(newHead: ClockHead, prevHead: ClockHead) {
     const ogHead = this.head.sort((a, b) => a.toString().localeCompare(b.toString()))
     newHead = newHead.sort((a, b) => a.toString().localeCompare(b.toString()))
     newHead.map(async cid => {
@@ -73,21 +57,17 @@ export class CRDTClock {
       }
     })
     if (ogHead.toString() === newHead.toString()) {
-      // this.notifyWatchers(updates || [])
       return
     }
     const ogPrev = prevHead.sort((a, b) => a.toString().localeCompare(b.toString()))
     if (ogHead.toString() === ogPrev.toString()) {
       this.setHead(newHead)
-      // this.notifyWatchers(updates || [])
       return
     }
     let head = this.head
     // const noLoader = this.head.length === 1 && !updates?.length
     const noLoader = false
-    const withBlocks = async (
-      fn: (blocks: Transaction) => Promise<BulkResult>
-    ) => {
+    const withBlocks = async (fn: (blocks: Transaction) => Promise<BulkResult>) => {
       if (!this.blocks) throw new Error('missing blocks')
       return await this.blocks.transaction(fn, undefined, { noLoader })
     }
