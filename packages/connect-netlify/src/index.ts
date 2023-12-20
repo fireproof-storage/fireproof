16fbc53a1ad227b7cd304406c94d80e3acd7e715
Chris Anderson <jchris@gmail.com>
2023-12-19 18:06:38 -0800

    connect netlify

--- packages/connect-netlify/src/index.ts
+++ packages/connect-netlify/src/index.ts
@@ -2,7 +2,25 @@ import { ConnectS3 } from '@fireproof/connect'
 import type { Database } from '@fireproof/core'
 import { ConnectNetlify } from './connect-netlify'
 
-// const netlifyCxs = new Map<string, ConnectNetlify>()
+const netlifyCxs = new Map<string, ConnectNetlify>()
 
 export { ConnectNetlify }
 
+export const connect = {
+  netlify: (db: Database, partyHost?: string, refresh?: boolean) => {
+    const {
+      name,
+      _crdt: {
+        blocks: { loader }
+      }
+    } = db
+    if (!name) throw new Error('database name is required')
+    if (!refresh && netlifyCxs.has(name)) {
+      return netlifyCxs.get(name)!
+    }
+    const connection = new ConnectNetlify(name)
+    connection.connect(loader!)
+    netlifyCxs.set(name, connection)
+    return connection
+  }
+}
