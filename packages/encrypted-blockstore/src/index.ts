23c73d666e4de69be4274f8b1c4799a5546a36c9
Chris Anderson <jchris@gmail.com>
2024-01-19 16:21:05 -0800

    bump versions again

--- packages/encrypted-blockstore/src/index.ts
+++ packages/encrypted-blockstore/src/index.ts
@@ -1,4 +1,4 @@
 export { EncryptedBlockstore, CompactionFetcher, BlockFetcher, CarTransaction, TransactionMeta } from './transaction'
 export { Loader } from './loader'
 export { DataStore, MetaStore } from './store'
-export { DbMeta } from "./types"
+export { DbMeta, AnyBlock, AnyLink } from "./types"
