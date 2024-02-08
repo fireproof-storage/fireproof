fb3bf2622a8aa493c8f5b79c06c1a850cd05fd0a
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2024-02-09 02:40:22 +0530

    changing metaUpload and download functions to make it compatible with new S3 connector

--- packages/connect/src/connect-s3.ts
+++ packages/connect/src/connect-s3.ts
@@ -1,102 +1,113 @@
-import { DownloadMetaFnParams, DownloadDataFnParams, UploadMetaFnParams, UploadDataFnParams } from './types'
-import { Connection } from "./connection"
-import fetch from "cross-fetch"
-import { Base64 } from "js-base64"
+import {
+  DownloadMetaFnParams,
+  DownloadDataFnParams,
+  UploadMetaFnParams,
+  UploadDataFnParams,
+} from "./types";
+import { Connection } from "./connection";
+import fetch from "cross-fetch";
 
 export class ConnectS3 extends Connection {
-  uploadUrl: URL
-  downloadUrl: URL
-  ws: WebSocket
-  messagePromise: Promise<Uint8Array[]>
-  messageResolve?: (value: Uint8Array[] | PromiseLike<Uint8Array[]>) => void
-
+  uploadUrl: URL;
+  downloadUrl: URL;
+  ws: WebSocket;
+  messagePromise: Promise<Uint8Array[]>;
+  messageResolve?: (value: Uint8Array[] | PromiseLike<Uint8Array[]>) => void;
 
   constructor(upload: string, download: string, websocket: string) {
-    console.log("The constructor is being called")
-    super()
-    this.uploadUrl = new URL(upload)
-    this.downloadUrl = new URL(download)
-    console.log('This is the websocket API', websocket)
-    this.ws = new WebSocket(websocket)
+    console.log("The constructor is being called");
+    super();
+    this.uploadUrl = new URL(upload);
+    this.downloadUrl = new URL(download);
+    console.log("This is the websocket API", websocket);
+    this.ws = new WebSocket(websocket);
 
     this.messagePromise = new Promise<Uint8Array[]>((resolve, reject) => {
       this.messageResolve = resolve;
-    })
+    });
   }
 
   async dataUpload(bytes: Uint8Array, params: UploadDataFnParams) {
     // console.log('s3 dataUpload', params.car.toString())
-    const fetchUploadUrl = new URL(`?${new URLSearchParams({ cache: Math.random().toString(), ...params }).toString()}`, this.uploadUrl)
-    const response = await fetch(fetchUploadUrl)
+    const fetchUploadUrl = new URL(
+      `?${new URLSearchParams({
+        cache: Math.random().toString(),
+        ...params,
+      }).toString()}`,
+      this.uploadUrl
+    );
+    const response = await fetch(fetchUploadUrl);
     if (!response.ok) {
       // console.log('failed to get upload url for data', params, response)
-      throw new Error('failed to get upload url for data ' + new Date().toISOString() + ' ' + response.statusText)
+      throw new Error(
+        "failed to get upload url for data " +
+          new Date().toISOString() +
+          " " +
+          response.statusText
+      );
     }
-    const { uploadURL } = await response.json() as { uploadURL: string }
-    const done = await fetch(uploadURL, { method: 'PUT', body: bytes })
+    const { uploadURL } = (await response.json()) as { uploadURL: string };
+    const done = await fetch(uploadURL, { method: "PUT", body: bytes });
     // console.log('s3 dataUpload done', params.car.toString(), done)
-    if (!done.ok) throw new Error('failed to upload data ' + done.statusText)
+    if (!done.ok) throw new Error("failed to upload data " + done.statusText);
   }
 
   async metaUpload(bytes: Uint8Array, params: UploadMetaFnParams) {
-    const event = await this.createEventBlock(bytes)
-    const base64String = Base64.fromUint8Array(bytes)
-    const crdtEntry = {
-      cid: event.cid.toString(),
-      data: base64String,
-      parents: this.parents.map((p) => p.toString()),
+    const fetchUploadUrl = new URL(
+      `?${new URLSearchParams({ type: "meta", ...params }).toString()}`,
+      this.uploadUrl
+    );
+    const response = await fetch(fetchUploadUrl);
+    if (!response.ok) {
+      // console.log('failed to get upload url for meta', params, response)
+      throw new Error("failed to get upload url for meta");
     }
-    const fetchUploadUrl = new URL(`?${new URLSearchParams({ type: "meta", ...params }).toString()}`, this.uploadUrl)
-    const done = await fetch(fetchUploadUrl, {
-      method: "PUT",
-      body: JSON.stringify(crdtEntry),
-    })
-    const result = await done.json()
-    if (result.status != 201) {
-      throw new Error("failed to upload data " + JSON.parse(result.body).message)
-    }
-    this.parents = [event.cid]
+    const { uploadURL } = (await response.json()) as { uploadURL: string };
+    if (!uploadURL) throw new Error("missing uploadURL");
+    const done = await fetch(uploadURL, { method: "PUT", body: bytes });
+    if (!done.ok) throw new Error("failed to upload data " + done.statusText);
     return null;
   }
 
   async dataDownload(params: DownloadDataFnParams) {
-    const { type, name, car } = params
-    const fetchFromUrl = new URL(`${type}/${name}/${car}.car`, this.downloadUrl)
-    const response = await fetch(fetchFromUrl)
-    if (!response.ok) return null // throw new Error('failed to download data ' + response.statusText)
-    const bytes = new Uint8Array(await response.arrayBuffer())
-    return bytes
+    const { type, name, car } = params;
+    const fetchFromUrl = new URL(
+      `${type}/${name}/${car}.car`,
+      this.downloadUrl
+    );
+    const response = await fetch(fetchFromUrl);
+    if (!response.ok) return null; // throw new Error('failed to download data ' + response.statusText)
+    const bytes = new Uint8Array(await response.arrayBuffer());
+    return bytes;
   }
 
-
-
   async onConnect() {
-    console.log("Is the onconnect function being called?")
+    console.log("Is the onconnect function being called?");
     if (!this.loader || !this.taskManager) {
-      throw new Error("loader and taskManager must be set")
+      throw new Error("loader and taskManager must be set");
     }
     this.ws.addEventListener("message", async (event: any) => {
-      console.log("The event", event)
-      console.log("This is the data", JSON.parse(event.data))
+      console.log("The event", event);
+      console.log("This is the data", JSON.parse(event.data));
       const data = JSON.parse(event.data);
-      const bytes = Base64.toUint8Array(data.items[0].data)
+      const bytes = Base64.toUint8Array(data.items[0].data);
 
       const afn = async () => {
         console.log("Inside the afn");
-        const uint8ArrayBuffer = bytes as Uint8Array
-        const eventBlock = await this.createEventBlock(uint8ArrayBuffer)
+        const uint8ArrayBuffer = bytes as Uint8Array;
+        const eventBlock = await this.createEventBlock(uint8ArrayBuffer);
         // const eventBlock = await this.decodeEventBlock(uint8ArrayBuffer)
-        await this.taskManager!.handleEvent(eventBlock)
+        await this.taskManager!.handleEvent(eventBlock);
         // @ts-ignore
-        this.messageResolve?.([eventBlock.value.data.dbMeta as Uint8Array])
+        this.messageResolve?.([eventBlock.value.data.dbMeta as Uint8Array]);
         // add the cid to our parents so we delete it when we send the update
         this.parents.push(eventBlock.cid);
         setTimeout(() => {
           this.messagePromise = new Promise<Uint8Array[]>((resolve, reject) => {
-            this.messageResolve = resolve
-          })
-        }, 0)
-      }
+            this.messageResolve = resolve;
+          });
+        }, 0);
+      };
 
       void afn();
     });
@@ -141,22 +152,18 @@ export class ConnectS3 extends Connection {
    * @returns - Returns the metadata bytes as a Uint8Array or null if the fetch is unsuccessful.
    */
   async metaDownload(params: DownloadMetaFnParams) {
-    const { name, branch } = params
-    const fetchUploadUrl = new URL(`?${new URLSearchParams({ type: "meta", ...params }).toString()}`, this.uploadUrl)
-    const data = await fetch(fetchUploadUrl)
-    let response = await data.json()
-    if (response.status != 200) throw new Error("Failed to download data")
-    response = JSON.parse(response.body).items
-    const events = await Promise.all(
-      response.map(async (element: any) => {
-        const base64String = element.data
-        const bytes = Base64.toUint8Array(base64String)
-        return { cid: element.cid, bytes }
-      })
-    )
-    const cids = events.map((e) => e.cid)
-    const uniqueParentsMap = new Map([...this.parents, ...cids].map((p) => [p.toString(), p]))
-    this.parents = Array.from(uniqueParentsMap.values())
-    return events.map((e) => e.bytes)
+    const { name, branch } = params;
+    const fetchFromUrl = new URL(
+      `meta/${name}/${
+        branch + ".json?cache=" + Math.floor(Math.random() * 1000000)
+      }`,
+      this.downloadUrl
+    );
+    const response = await fetch(fetchFromUrl);
+    if (!response.ok) return null;
+    const bytes = new Uint8Array(await response.arrayBuffer());
+    // todo we could use a range list to make mvcc / crdt logic work in the s3 bucket
+    // we would name the meta files with a timestamp, eg using our UUIDv7 library
+    return [bytes];
   }
 }
