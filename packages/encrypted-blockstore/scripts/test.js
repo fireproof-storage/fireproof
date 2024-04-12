990ae7f5ef8f0aaa253dcc1be53bc683c0778c02
Dhruv Soni <dhruvsoni@Dhruvs-Laptop.local>
2024-04-13 02:52:16 +0530

    further improvements to test script

--- packages/encrypted-blockstore/scripts/test.js
+++ packages/encrypted-blockstore/scripts/test.js
@@ -1,6 +1,13 @@
 import { spawn } from 'child_process'
+import fs from "node:fs"
 const args = process.argv.slice(2)
 
+const testfiles=fs.readdirSync('test');
+if(testfiles.length==0)
+{
+  console.log('No files found to test');
+  process.exit(0);
+}
 let command = 'mocha test/*.js'
 
 if (args.length > 0) {
