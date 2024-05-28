7a60124aa6824b9b321004f4f0c6da7619fbc230
Travis Vachon <travis.vachon@gmail.com>
2024-05-27 18:18:07 -0700

    feat: get ipfs connector working
    introduce a new, "v2" connector and leave the old one in place - we
    may want to get rid of that too, soon?

---
+++ examples/next-app/src/app/page.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { Database, fireproof } from "@fireproof/core";
+import { connect } from "@fireproof/ipfs"
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import useSWR from 'swr'
+import useLocalStorageState from 'use-local-storage-state'
+
+type AuthFormValues = {
+  email: string
+}
+
+export default function Home () {
+  const [email, setEmail] = useLocalStorageState<string>('example-email')
+  const [db] = useState<Database>(fireproof('example'))
+  const [connection] = useState(connect.ipfs2(db, 'example-sync'))
+  const { register, handleSubmit } = useForm<AuthFormValues>();
+  const { data: docs, mutate: mutateTestThings } = useSWR(
+    'test things',
+    async () => db.query('test'),
+    {
+      refreshInterval: 1000
+    }
+  )
+  async function addThing () {
+    await db.put({ test: 'foo', time: Date.now() })
+    await mutateTestThings()
+  }
+
+  async function login (data: AuthFormValues) {
+    // set email - the change will be detected and auth will happen below
+    setEmail(data.email)
+  }
+
+  // this will run any time email changes
+  useEffect(function () {
+    if (email) {
+      (async () => {
+        console.debug(`authorizing as ${email}`)
+        await connection.authorize(email as `${string}@${string}`)
+        console.debug(`authorized as ${email}`)
+      })()
+    }
+  }, [email])
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {docs && docs.rows.map((r: any) => <pre key={r.id}>{JSON.stringify(r, null, 2)}</pre>)}
+      <button onClick={addThing}>Add test thing</button>
+      {email ? (
+        <div>logged in as {email}</div>
+      ) : (
+        <form onSubmit={handleSubmit((data: AuthFormValues) => login(data))}>
+          <input {...register("email")} placeholder="email" />
+          <input type="submit" />
+        </form>
+      )}
+    </main >
+  );
+}
--- packages/connect-ipfs/src/index.ts
+++ packages/connect-ipfs/src/index.ts
@@ -1,10 +1,24 @@
 import { ConnectIPFS, ConnectIPFSParams } from './connect-ipfs'
 import { ConnectS3 } from '@fireproof/aws'
 import type { Connectable } from '@fireproof/encrypted-blockstore'
+import { ConnectIPFSV2 } from './connect-ipfs-v2'
 
-const ipfsCxs = new Map<string, ConnectIPFS>()
+const ipfsCxs = new Map<string, ConnectIPFS | ConnectIPFSV2>()
 
 export const connect = {
+  ipfs2: ({ name, blockstore }: Connectable, schemaName?: string) => {
+    if (!name) throw new Error('database name is required')
+    if (ipfsCxs.has(name)) {
+      return ipfsCxs.get(name)!
+    }
+    if (!schemaName && location) {
+      schemaName = location.origin
+    }
+    const connection = new ConnectIPFSV2({ name, schema: schemaName! } as ConnectIPFSParams)
+    connection.connect(blockstore)
+    ipfsCxs.set(name, connection)
+    return connection
+  },
   ipfs: ({ name, blockstore }: Connectable, schemaName?: string) => {
     if (!name) throw new Error('database name is required')
     if (ipfsCxs.has(name)) {
@@ -30,7 +44,7 @@ export const connect = {
       upload: 'https://04rvvth2b4.execute-api.us-east-2.amazonaws.com/uploads',
       download: 'https://sam-app-s3uploadbucket-e6rv1dj2kydh.s3.us-east-2.amazonaws.com'
     }
-    const s3conn = new ConnectS3(s3conf.upload, s3conf.download,'')
+    const s3conn = new ConnectS3(s3conf.upload, s3conf.download, '')
     s3conn.connectStorage(blockstore)
     const ipfsConn = new ConnectIPFS({ name, schema: schemaName! } as ConnectIPFSParams)
     ipfsConn.connectMeta(blockstore)
