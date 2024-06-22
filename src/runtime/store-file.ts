407f9a50b7164dc721b559280d68e307526c4ffb
Chris Anderson <jchris@gmail.com>
2024-06-22 11:29:29 -0700

    almost all passing

--- src/runtime/store-file.ts
+++ src/runtime/store-file.ts
@@ -68,9 +68,9 @@ export class FileDataStore extends DataStore {
     }
 
     async save(car: AnyBlock): Promise<void> {
-        const filepath = this.cidPath(car.cid);
-        console.log("save->", filepath);
-        await writePathFile(filepath, car.bytes);
+      const filepath = this.cidPath(car.cid);
+      // console.log("save->", filepath);
+      await writePathFile(filepath, car.bytes);
     }
 
     private cidPath(cid: AnyLink) {
