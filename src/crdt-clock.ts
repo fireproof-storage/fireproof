a09549403bd830e03cfbf358785484d5dad383b6
Chris Anderson <jchris@gmail.com>
2024-06-25 08:17:39 -0700

    fix await chain

--- src/crdt-clock.ts
+++ src/crdt-clock.ts
@@ -34,7 +34,7 @@ export class CRDTClock<T extends DocTypes> {
       prevHead,
       updates,
     })) {
-      this.processUpdates(updatesAcc, all, prevHead);
+      return this.processUpdates(updatesAcc, all, prevHead);
     }
   }
 
