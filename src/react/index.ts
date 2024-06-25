987009b2ee2ae466a2c0b492ee6933493690b812
Meno Abels <meno.abels@adviser.com>
2024-06-25 16:47:07 +0200

    chore: improve react

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,4 +1,5 @@
 export { type TLUseDocument, useDocument } from "./useDocument";
 export { FireproofCtx, type UseFireproof, useFireproof } from "./useFireproof";
 export { type TLUseLiveQuery, useLiveQuery } from "./useLiveQuery";
+// why is this there is should be a package system
 export * from "@fireproof/core";
