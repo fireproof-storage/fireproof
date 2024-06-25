dbf7867604b9db47a8dd508212354e61431f9e6a
Meno Abels <meno.abels@adviser.com>
2024-06-25 18:03:39 +0200

    chore: prettier + lint

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     exclude: ["tests/react/**"],
     include: ["tests/**/*test.?(c|m)[jt]s?(x)"],
-    globals: true
+    globals: true,
   },
-})
+});
