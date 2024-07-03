4d4c7f7fe186b19aa925aac9d391d3538589836b
Meno Abels <meno.abels@adviser.com>
2024-07-03 10:31:13 +0200

    chore: vitest is nasty on the first run

--- smoke/react/it.sh
+++ smoke/react/it.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 cd smoke/react 
-rm -rf node_modules dist
+rm -rf node_modules dist pnpm-lock.yaml
+cp package-template.json package.json
 pnpm install
 pnpm install -f ../../dist/use-fireproof/use-fireproof-*.tgz
+pnpm run test > /dev/null 2>&1 || true
 pnpm run test
