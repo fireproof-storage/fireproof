fffd8924e9a86ef2808a39252253347e15e28d64
Meno Abels <meno.abels@adviser.com>
2024-07-03 18:08:52 +0200

    chore: enable the connectors

--- src/block-store/index.ts
+++ src/block-store/index.ts
@@ -25,10 +25,13 @@ export * from "./store-factory.js";
 import { type CarClockHead, type Connectable, type DbMetaEventBlock } from "./connection-base.js";
 export { ConnectREST, CarClockHead, Connectable, DbMetaEventBlock };
 
-export { EncryptedBlockstore, BaseBlockstore, CompactionFetcher, type BlockFetcher, CarTransaction } from "./transaction";
-export { Loader, Loadable } from "./loader";
-export { DataStore, MetaStore } from "./store";
-export { RemoteWAL, type WALState } from "./remote-wal";
+export { EncryptedBlockstore, BaseBlockstore, CompactionFetcher, type BlockFetcher, CarTransaction } from "./transaction.js";
+export { Loader, Loadable } from "./loader.js";
+export { DataStore, MetaStore } from "./store.js";
+export { RemoteWAL, type WALState } from "./remote-wal.js";
+export { parseCarFile } from "./loader-helpers.js";
+export { ConnectionBase } from "./connection-base.js";
+
 export {
   DbMeta,
   AnyBlock,
