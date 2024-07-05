a3fee28968a347127d0a296b485f509c895b24b1
Chris Anderson <jchris@gmail.com>
2024-07-05 11:33:35 -0700

    pnpm format

--- tests/fireproof/config.test.ts
+++ tests/fireproof/config.test.ts
@@ -5,7 +5,7 @@ import { SysContainer } from "../../src/runtime";
 describe("fireproof/config", () => {
   beforeAll(async () => {
     await rt.SysContainer.start();
-  })
+  });
 
   if (!isNode) {
     it("default", async () => {
@@ -24,9 +24,14 @@ describe("fireproof/config", () => {
   it("node default", async () => {
     const old = process.env.FP_STORAGE_URL;
     delete process.env.FP_STORAGE_URL;
-    let baseDir = rt.dataDir("my-app").replace(/\?.*$/, "").replace(/^file:\/\//, "");
+    let baseDir = rt
+      .dataDir("my-app")
+      .replace(/\?.*$/, "")
+      .replace(/^file:\/\//, "");
     baseDir = SysContainer.join(baseDir, rt.FILESTORE_VERSION, "my-app");
-    await SysContainer.rm(baseDir, { recursive: true }).catch(() => { /* */ });
+    await SysContainer.rm(baseDir, { recursive: true }).catch(() => {
+      /* */
+    });
 
     expect(baseDir).toMatch(new RegExp(`/\\.fireproof/${rt.FILESTORE_VERSION}/my-app`));
 
@@ -35,12 +40,10 @@ describe("fireproof/config", () => {
     expect(db.name).toBe("my-app");
     const carStore = await db.blockstore.loader?.carStore();
 
-
     expect(carStore?.url.toString()).toMatch(
       new RegExp(`file:.*\\/\\.fireproof\\?name=my-app&store=data&version=${rt.FILESTORE_VERSION}`),
     );
     expect((await SysContainer.stat(SysContainer.join(baseDir, "data"))).isDirectory()).toBeTruthy();
-
 
     const fileStore = await db.blockstore.loader?.fileStore();
     expect(fileStore?.url.toString()).toMatch(
@@ -58,16 +61,21 @@ describe("fireproof/config", () => {
     const old = process.env.FP_STORAGE_URL;
     process.env.FP_STORAGE_URL = "./dist/env";
 
-    let baseDir = rt.dataDir("my-app").replace(/\?.*$/, "").replace(/^file:\/\//, "");
+    let baseDir = rt
+      .dataDir("my-app")
+      .replace(/\?.*$/, "")
+      .replace(/^file:\/\//, "");
     baseDir = SysContainer.join(baseDir, rt.FILESTORE_VERSION, "my-app");
-    await SysContainer.rm(baseDir, { recursive: true }).catch(() => { /* */ });
+    await SysContainer.rm(baseDir, { recursive: true }).catch(() => {
+      /* */
+    });
 
     const db = fireproof("my-app");
     await db.put({ name: "my-app" });
     expect(db.name).toBe("my-app");
     const carStore = await db.blockstore.loader?.carStore();
     expect(carStore?.url.toString()).toMatch(
-      new RegExp(`file://\\./dist/env\\?name=my-app&store=data&version=${rt.FILESTORE_VERSION}`)
+      new RegExp(`file://\\./dist/env\\?name=my-app&store=data&version=${rt.FILESTORE_VERSION}`),
     );
     expect((await SysContainer.stat(SysContainer.join(baseDir, "data"))).isDirectory()).toBeTruthy();
     const fileStore = await db.blockstore.loader?.fileStore();
@@ -83,11 +91,11 @@ describe("fireproof/config", () => {
   });
 
   it("file path", async () => {
-
     let baseDir = "./dist/data".replace(/\?.*$/, "").replace(/^file:\/\//, "");
     baseDir = SysContainer.join(baseDir, rt.FILESTORE_VERSION, "my-app");
-    await SysContainer.rm(baseDir, { recursive: true }).catch(() => { /* */ });;
-
+    await SysContainer.rm(baseDir, { recursive: true }).catch(() => {
+      /* */
+    });
 
     const db = fireproof("my-app", {
       store: {
@@ -118,7 +126,9 @@ describe("fireproof/config", () => {
   it("sqlite path", async () => {
     let dbFile = "sqlite://./dist/sqlite".replace(/\?.*$/, "").replace(/^sqlite:\/\//, "");
     dbFile = SysContainer.join(dbFile, "my-app.sqlite");
-    await SysContainer.rm(dbFile, { recursive: true }).catch(() => { /* */ });;
+    await SysContainer.rm(dbFile, { recursive: true }).catch(() => {
+      /* */
+    });
 
     const db = fireproof("my-app", {
       store: {
@@ -133,7 +143,8 @@ describe("fireproof/config", () => {
     expect(db.name).toBe("my-app");
     const carStore = await db.blockstore.loader?.carStore();
     expect(carStore?.url.toString()).toMatch(
-      new RegExp(`sqlite://./dist/sqlite\\?name=my-app&store=data&version=${rt.SQLITE_VERSION}`));
+      new RegExp(`sqlite://./dist/sqlite\\?name=my-app&store=data&version=${rt.SQLITE_VERSION}`),
+    );
     const fileStore = await db.blockstore.loader?.fileStore();
     expect(fileStore?.url.toString()).toMatch(
       new RegExp(`sqlite://./dist/sqlite\\?name=my-app&store=data&version=${rt.SQLITE_VERSION}`),
@@ -144,7 +155,6 @@ describe("fireproof/config", () => {
     );
   });
 
-
   it("full config path", async () => {
     const db = fireproof("my-app", {
       store: {
@@ -154,7 +164,7 @@ describe("fireproof/config", () => {
           meta: "sqlite://./dist/sqlite/meta",
           data: "sqlite://./dist/sqlite/data",
           index: "sqlite://./dist/sqlite/index",
-          remoteWAL: "sqlite://./dist/sqlite/wal"
+          remoteWAL: "sqlite://./dist/sqlite/wal",
         },
       },
     });
@@ -165,17 +175,12 @@ describe("fireproof/config", () => {
     const carStore = await db.blockstore.loader?.carStore();
     expect(carStore?.url.toString()).toMatch(
       // sqlite://./dist/sqlite/data?store=data&version=v0.19-sqlite
-      new RegExp(`sqlite://./dist/sqlite/data\\?store=data&version=${rt.SQLITE_VERSION}`));
-
-    const fileStore = await db.blockstore.loader?.fileStore();
-    expect(fileStore?.url.toString()).toMatch(
       new RegExp(`sqlite://./dist/sqlite/data\\?store=data&version=${rt.SQLITE_VERSION}`),
     );
+
+    const fileStore = await db.blockstore.loader?.fileStore();
+    expect(fileStore?.url.toString()).toMatch(new RegExp(`sqlite://./dist/sqlite/data\\?store=data&version=${rt.SQLITE_VERSION}`));
     const metaStore = await db.blockstore.loader?.metaStore();
-    expect(metaStore?.url.toString()).toMatch(
-      new RegExp(`sqlite://./dist/sqlite/meta\\?store=meta&version=${rt.SQLITE_VERSION}`),
-    );
+    expect(metaStore?.url.toString()).toMatch(new RegExp(`sqlite://./dist/sqlite/meta\\?store=meta&version=${rt.SQLITE_VERSION}`));
   });
-
-
 });
