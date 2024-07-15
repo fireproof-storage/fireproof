9c44265ae1eb06e4c7ce3706a53dd4285a59a964
Chris Anderson <jchris@gmail.com>
2024-07-15 15:17:02 -0700

    add @fireproof/core to use-fireproof dependencies

---
+++ version-use-fireproof-dep.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import process from "process";
+import path from "path";
+
+async function main() {
+  const mainPackageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
+  const files = process.argv.slice(2);
+  for (const file of files) {
+    const destPath = path.join(path.dirname(file), "package.json");
+    fs.copyFileSync(path.basename(file), destPath);
+    const destPackageJson = JSON.parse(fs.readFileSync(path.basename(file), "utf8"));
+
+    destPackageJson.dependencies["@fireproof/core"] = mainPackageJson.version
+
+    destPackageJson.version = mainPackageJson.version;
+    fs.writeFileSync(destPath, JSON.stringify(destPackageJson, null, 2));
+    console.log(`Copied ${file} to ${destPath} with version ${destPackageJson.version}`);
+  }
+}
+
+main().catch(console.error);
