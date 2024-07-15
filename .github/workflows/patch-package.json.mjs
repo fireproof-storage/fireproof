8f5ff4f1cb4224fd54d0ea381c2b40d20521fa75
Chris Anderson <jchris@gmail.com>
2024-07-15 14:05:12 -0700

    version patch

--- .github/workflows/patch-package.json.mjs
+++ .github/workflows/patch-package.json.mjs
@@ -5,4 +5,5 @@ let version = process.argv[process.argv.length - 1];
 version = version.split("/").slice(-1)[0].replace(/^v/, "");
 console.error(`Patch package.json version to ${version}`);
 const packageJson = JSON.parse(fs.readFileSync("package.json").toString());
+packageJson.version = version;
 fs.writeFileSync("package.json", JSON.stringify(packageJson, undefined, 2) + "\n");
