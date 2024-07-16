b2269085b774a6afdb80a3aa81ecba074af2a137
Chris Anderson <jchris@gmail.com>
2024-07-16 13:12:51 -0700

    export allDocs

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,5 +1,6 @@
 export { type TLUseDocument, useDocument } from "./useDocument";
 export { FireproofCtx, type UseFireproof, useFireproof } from "./useFireproof";
 export { type TLUseLiveQuery, useLiveQuery } from "./useLiveQuery";
+export { type TLUseAllDocs, useAllDocs } from "./useAllDocs";
 // why is this there is should be a package system
 export * from "@fireproof/core";
