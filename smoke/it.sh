89ce79ffc8cf96d67b68baa408d1a124c5cc9833
Meno Abels <meno.abels@adviser.com>
2024-07-17 09:34:09 +0200

    chore: smoke don't ref the node_modules -> hunt std-env

--- smoke/it.sh
+++ smoke/it.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 cd smoke 
-rm -rf package.json node_modules
+smokeDir=$(pwd)
+tmpDir=$(mktemp -d)
+cp * $tmpDir || true
+cd $tmpDir
+rm -rf pnpm-lock.yaml package.json node_modules
 pnpm init 
 node patch-package.json.mjs 
-pnpm install -f ../dist/fireproof-core/fireproof-core-*.tgz
+pnpm install -f $smokeDir/../dist/fireproof-core/fireproof-core-*.tgz
 npx tsx ./node-test.ts
+rm -rf $tmpDir
--- smoke/react/it.sh
+++ smoke/react/it.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 set -e
 cd smoke/react 
+smokeDir=$(pwd)
+tmpDir=$(mktemp -d)
 rm -rf node_modules dist pnpm-lock.yaml
+cp -pr * $tmpDir
+cd $tmpDir
 cp package-template.json package.json
 pnpm install
-pnpm install -f ../../dist/use-fireproof/use-fireproof-*.tgz
+pnpm install -f $smokeDir/../../dist/use-fireproof/use-fireproof-*.tgz
 # pnpm run test > /dev/null 2>&1 || true
 pnpm run test
+rm -rf $tmpDir
