e0372fdf2d35a8b5ba32d968bde7a033a8d509b3
Chris Anderson <jchris@gmail.com>
2024-07-18 09:12:13 -0700

    format

--- src/blockstore/store-factory.ts
+++ src/blockstore/store-factory.ts
@@ -67,7 +67,9 @@ export function registerStoreProtocol(item: StoreFactoryItem) {
     if (!item.overrideBaseURL && storeFactory.get(protocol) !== item) {
       const logger = ensureLogger({}, "registerStoreProtocol", { protocol });
       logger.Warn().Msg(`protocol ${protocol} already registered`);
-      return () => { /* no-op */ };
+      return () => {
+        /* no-op */
+      };
     }
   }
   // we need to clear the overrideBaseURL if it is set
