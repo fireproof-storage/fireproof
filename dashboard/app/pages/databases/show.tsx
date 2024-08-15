320d37ab16a2e4d29297b8a9c30688a8dc1e7c76
Selem Delul <selem@selem.im>
2024-08-15 10:47:44 -0400

    Placeholder cleanup

--- dashboard/app/pages/databases/show.tsx
+++ dashboard/app/pages/databases/show.tsx
@@ -3,12 +3,11 @@ import { useFireproof } from "use-fireproof";
 import DynamicTable from "../../components/DynamicTable";
 import { headersForDocs } from "../../components/dynamicTableHelpers";
 
-
 export default function DbInfo() {
   const { name } = useParams();
   const { useLiveQuery, database } = useFireproof(name);
   const allDocs = useLiveQuery("_id");
-  console.log(allDocs, database)
+  console.log(allDocs, database);
   const docs = allDocs.docs.filter((doc) => doc);
 
   const headers = headersForDocs(docs);
