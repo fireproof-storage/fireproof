91b71f2ee88fa40e6f75fe1436dbb80a4c46b3d4
Chris Anderson <jchris@gmail.com>
2024-09-10 18:17:27 -0700

    subscribe

--- src/blockstore/fragment-gateway.ts
+++ src/blockstore/fragment-gateway.ts
@@ -167,6 +167,13 @@ export class FragmentGateway implements bs.Gateway {
     return Result.Ok(buffer || new Uint8Array(0));
   }
 
+  async subscribe(url: URI, callback: (msg: Uint8Array) => void): Promise<void> {
+    if (!this.innerGW.subscribe) {
+      throw this.logger.Error().Msg("Subscribe not supported").AsError();
+    }
+    return this.innerGW.subscribe(url, callback);
+  }
+
   async delete(url: URI): Promise<bs.VoidResult> {
     const rfrags = await getFrags(url, this.innerGW, this.headerSize, this.logger);
     for (const rfrag of rfrags) {
