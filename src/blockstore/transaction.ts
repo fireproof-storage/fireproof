de0a1b0736956209b58bfa185b61786e23b7d0c1
Chris Anderson <jchris@gmail.com>
2024-10-12 17:50:06 -0700

    try noLoader for groupin transactions

--- src/blockstore/transaction.ts
+++ src/blockstore/transaction.ts
@@ -26,11 +26,6 @@ export interface CarTransactionOpts {
   readonly noLoader: boolean;
 }
 
-export interface CarTransactionOpts {
-  readonly add: boolean;
-  readonly noLoader: boolean;
-}
-
 export class CarTransaction extends MemoryBlockstore implements CarMakeable {
   readonly parent: BaseBlockstore;
   constructor(parent: BaseBlockstore, opts: CarTransactionOpts = { add: true, noLoader: false }) {
@@ -157,11 +152,7 @@ export class BaseBlockstore implements BlockFetcher {
     return new CarTransaction(this, opts);
   }
 
-  async commitTransaction<M extends TransactionMeta>(
-    t: CarTransaction,
-    done: M,
-    opts: CarTransactionOpts,
-  ): Promise<TransactionWrapper<M>> {
+  async commitTransaction<M extends TransactionMeta>(t: CarTransaction, done: M, opts: CarTransactionOpts): Promise<TransactionWrapper<M>> {
     if (!this.loader) throw this.logger.Error().Msg("loader required to commit").AsError();
     const cars = await this.loader?.commit<M>(t, done, opts);
     if (this.ebOpts.autoCompact && this.loader.carLog.length > this.ebOpts.autoCompact) {
