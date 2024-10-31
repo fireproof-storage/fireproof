bd8daea304e6af8b9a866dec35e0df8fac7b677c
Meno Abels <meno.abels@adviser.com>
2024-10-31 14:34:26 +0100

    chore: fp/core loaded runtime specifics dynamic

--- src/runtime/gateways/file/utils.ts
+++ src/runtime/gateways/file/utils.ts
@@ -1,59 +1,7 @@
-import { KeyedResolvOnce, URI } from "@adviser/cement";
+import { URI } from "@adviser/cement";
 import { getStore } from "../../../utils.js";
-import { PARAM, SuperThis, SysFileSystem } from "../../../types.js";
-
-const externalLoaders = new KeyedResolvOnce<SysFileSystem>();
-export async function getFileSystem(url: URI): Promise<SysFileSystem> {
-  const name = url.getParam("fs", "node");
-  let fs: SysFileSystem;
-
-  if (runtimeFn().isDeno) {
-    const { DenoFileSystem } = await import("./deno-filesystem.js");
-    fs = new DenoFileSystem();
-  } else if (runtimeFn().isNodeIsh) {
-    const { NodeFileSystem } = await import("./node-filesystem.js");
-    fs = new NodeFileSystem();
-  } else {
-    throw new Error("unsupported runtime");
-
-  switch (name) {
-    case "mem":
-      fs = await externalLoaders.get(name).once(async () => {
-        // const memjs = "./node/mem-filesystem.js"
-        // const { MemFileSystem } = await import(/* @vite-ignore */memjs);
-        const { MemFileSystem } = await import("./node/mem-filesystem.js");
-        return new MemFileSystem();
-      });
-      break;
-    // case 'deno': {
-    //   const { DenoFileSystem } = await import("./deno-filesystem.js");
-    //   fs = new DenoFileSystem();
-    //   break;
-    // }
-    case "node": {
-      const { NodeFileSystem } = await import("./node-filesystem@skip-iife.js");
-      fs = new NodeFileSystem();
-      break;
-    }
-    case "sys":
-    default: {
-      // if (runtimeFn().isDeno) {
-      //   return getFileSystem(url.build().setParam("fs", "deno").URI());
-      // } else  {
-      return getFileSystem(url.build().setParam("fs", "node").URI());
-      // }
-    }
-    default:
-      fs = await externalLoaders.get(name).once(async () => {
-        // const nodejs = "./node/node-filesystem.js"
-        // const { NodeFileSystem } = await import(/* @vite-ignore */nodejs);
-        const { NodeFileSystem } = await import("./node/node-filesystem.js");
-        return new NodeFileSystem();
-      });
-
-  }
-  return fs.start();
-}
+import { SuperThis } from "../../../types.js";
+export * from "./get-file-system-static.js";
 
 export function getPath(url: URI, sthis: SuperThis): string {
   const basePath = url.pathname;
