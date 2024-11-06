73e16cae4863b89512b591757f45b2489b6a1595
Selem Delul <selem@fireproof.storage>
2024-11-06 15:10:41 -0500

    Deprecate top level react hooks.

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,7 +1,23 @@
-export { type TLUseDocument, useDocument } from "./useDocument";
+export {
+  type TLUseDocument,
+  /** @deprecated Use return values from useFireproof('dbname') instead. Top level usage will be removed in future versions. */
+  useDocument,
+} from "./useDocument";
 export { FireproofCtx, type UseFireproof, useFireproof } from "./useFireproof";
-export { type TLUseLiveQuery, useLiveQuery } from "./useLiveQuery";
-export { type TLUseAllDocs, useAllDocs } from "./useAllDocs";
-export { type TLUseChanges, useChanges } from "./useChanges";
+export {
+  type TLUseLiveQuery,
+  /** @deprecated Use return values from useFireproof('dbname') instead. Top level usage will be removed in future versions. */
+  useLiveQuery,
+} from "./useLiveQuery";
+export {
+  type TLUseAllDocs,
+  /** @deprecated Use return values from useFireproof('dbname') instead. Top level usage will be removed in future versions. */
+  useAllDocs,
+} from "./useAllDocs";
+export {
+  type TLUseChanges,
+  /** @deprecated Use return values from useFireproof('dbname') instead. Top level usage will be removed in future versions. */
+  useChanges,
+} from "./useChanges";
 // why is this there is should be a package system
 export * from "@fireproof/core";
