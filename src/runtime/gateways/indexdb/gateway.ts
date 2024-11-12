1e31f5965538875d4ee47793e783a80f63d26249
Selem Delul <selem@fireproof.storage>
2024-11-12 14:39:51 -0500

    Relax indexeddb name sanitization to include hypens
    Probably can include more characters but for now it would be nice to have at least hypen for a valid char since uuids are used for remote names by default.
    
    on dashboard, indexeddb is used as source of truth for the database list

--- src/runtime/gateways/indexdb/gateway.ts
+++ src/runtime/gateways/indexdb/gateway.ts
@@ -1,7 +1,88 @@
-import { ResolveOnce, Result, URI } from "@adviser/cement";
-import { Gateway, GetResult, TestGateway, VoidResult } from "../../../blockstore/gateway.js";
-import { SuperThis } from "../../../types.js";
-import { gatewayImport } from "./gateway-import-static.js";
+import { openDB, IDBPDatabase } from "idb";
+import { exception2Result, KeyedResolvOnce, Logger, Result, URI } from "@adviser/cement";
+
+import { INDEXDB_VERSION } from "./version.js";
+import { ensureLogger, exceptionWrapper, getKey, getStore, NotFoundError } from "../../../utils.js";
+import { Gateway, GetResult } from "../../../blockstore/gateway.js";
+import { PARAM, SuperThis } from "../../../types.js";
+import { FPEnvelope } from "../../../blockstore/fp-envelope.js";
+import { fpDeserialize, fpSerialize } from "../fp-envelope-serialize.js";
+
+function ensureVersion(url: URI): URI {
+  return url.build().defParam(PARAM.VERSION, INDEXDB_VERSION).URI();
+}
+
+interface IDBConn {
+  readonly db: IDBPDatabase<unknown>;
+  readonly dbName: DbName;
+  readonly version: string;
+  readonly url: URI;
+}
+const onceIndexDB = new KeyedResolvOnce<IDBConn>();
+
+function sanitzeKey(key: string | string[]): string | string[] {
+  if (key.length === 1) {
+    key = key[0];
+  }
+  return key;
+}
+
+async function connectIdb(url: URI, sthis: SuperThis): Promise<IDBConn> {
+  const dbName = getIndexDBName(url, sthis);
+  const once = await onceIndexDB.get(dbName.fullDb).once(async () => {
+    const db = await openDB(dbName.fullDb, 1, {
+      upgrade(db) {
+        ["version", "data", "wal", "meta", "idx.data", "idx.wal", "idx.meta"].map((store) => {
+          db.createObjectStore(store, {
+            autoIncrement: false,
+          });
+        });
+      },
+    });
+    const found = await db.get("version", "version");
+    const version = ensureVersion(url).getParam(PARAM.VERSION) as string;
+    if (!found) {
+      await db.put("version", { version }, "version");
+    } else if (found.version !== version) {
+      sthis.logger.Warn().Url(url).Str("version", version).Str("found", found.version).Msg("version mismatch");
+    }
+    return { db, dbName, version, url };
+  });
+  return {
+    ...once,
+    url: url.build().setParam(PARAM.VERSION, once.version).URI(),
+  };
+}
+
+export interface DbName {
+  readonly fullDb: string;
+  readonly objStore: string;
+  readonly connectionKey: string;
+  readonly dbName: string;
+}
+
+function joinDBName(...names: string[]): string {
+  return names
+    .map((i) => i.replace(/^[^a-zA-Z0-9]+/g, "").replace(/[^a-zA-Z0-9-]+/g, "_"))
+    .filter((i) => i.length)
+    .join(".");
+}
+
+export function getIndexDBName(iurl: URI, sthis: SuperThis): DbName {
+  const url = ensureVersion(iurl);
+  const fullDb = url.pathname.replace(/^\/+/, "").replace(/\?.*$/, ""); // cut leading slashes
+  const dbName = url.getParam(PARAM.NAME);
+  if (!dbName) throw sthis.logger.Error().Str("url", url.toString()).Msg(`name not found`).AsError();
+  const result = joinDBName(fullDb, dbName);
+  const objStore = getStore(url, sthis, joinDBName).name;
+  const connectionKey = [result, objStore].join(":");
+  return {
+    fullDb: result,
+    objStore,
+    connectionKey,
+    dbName,
+  };
+}
 
 const loadExternal = new ResolveOnce<Gateway>();
 export class IndexDBGateway implements Gateway {
