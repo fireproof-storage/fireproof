d7cfd89b492e6792ca78e56c71526c160b3b67b7
Meno Abels <meno.abels@adviser.com>
2024-11-15 06:56:36 +0100

    chore: transition to forwarding use-fireproof chore: add forwarding package entry

--- smoke/react/it.sh
+++ smoke/react/it.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 set -e
 projectRoot=$(pwd)
-cd smoke/react 
+cd smoke/react
 smokeDir=$(pwd)
 tmpDir=$(mktemp -d)
 rm -rf node_modules dist pnpm-lock.yaml
 cp -pr * $tmpDir
 cd $tmpDir
-cp $projectRoot/dist/npmrc-smoke .npmrc
-cp package-template.json package.json
-pnpm install --registry=http://localhost:4873/
+fp_core=$(echo $smokeDir/../../dist/fireproof-core/fireproof-core-*.tgz)
+use_fp=$(echo $smokeDir/../../dist/use-fireproof/use-fireproof-*.tgz)
+sed -e "s|FIREPROOF_CORE|file://$fp_core|g" \
+    -e "s|USE_FIREPROOF|file://$use_fp|g" package-template.json > package.json
+# cp package-template.json package.json
+pnpm install
+# pnpm install -f "file://$smokeDir/../../dist/fireproof-core/fireproof-core-*.tgz"
+# pnpm install -f "file://$smokeDir/../../dist/use-fireproof/use-fireproof-*.tgz"
 # pnpm run test > /dev/null 2>&1 || true
 pnpm run test
 rm -rf $tmpDir
