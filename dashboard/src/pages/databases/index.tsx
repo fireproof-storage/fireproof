d63ee397582ddf14d2d468bc868c076c268b4be6
Selem Delul <selem@fireproof.storage>
2024-11-21 14:32:07 -0500

    Highlight react tutorial link.

--- dashboard/src/pages/databases/index.tsx
+++ dashboard/src/pages/databases/index.tsx
@@ -3,8 +3,15 @@ export default function DatabaseIndex() {
     <div className="flex flex-col items-center justify-center h-full">
       <h2 className="text-2xl font-semibold">Welcome to Fireproof</h2>
       <p className="text-muted-foreground">
-        The left sidebar lists database you have synced before. To get started writing apps, 
-        try <a href="https://use-fireproof.com/docs/react-tutorial/">the React tutorial</a>.
+        The left sidebar lists database you have created before. To get started
+        writing apps, try{" "}
+        <a
+          href="https://use-fireproof.com/docs/react-tutorial/"
+          className="text-[--accent] hover:underline"
+        >
+          the React tutorial
+        </a>
+        .
       </p>
     </div>
   );
