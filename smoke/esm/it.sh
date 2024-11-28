a8db7dda89b7128df74a9910132a6d614691c015
Meno Abels <meno.abels@adviser.com>
2024-11-28 16:50:05 +0100

    Iife ci vitest (#398)
    * test: iife build using playwright
    * test: Tweaks to run playwright tests in CI
    * chore: move iife -> esm
    * chore: now the esm.sh local backend works
    * fix: the selection docker-compose/docker compose
    * chore: iife -> esm
    ---------
    
    Co-authored-by: Steven Vandevelde <steven@tokono.ma>

--- smoke/esm/it.sh
+++ smoke/esm/it.sh
@@ -1,16 +1,39 @@
 #!/bin/sh
 set -e
-projectRoot=$(pwd)
 cd smoke/esm
 smokeDir=$(pwd)
+
+if which docker-compose
+then
+  dockerCompose="docker-compose"
+else
+  dockerCompose="docker compose"
+fi
+$dockerCompose up -d
+packageDir=../../dist/fireproof-core
+
+token=$(curl \
+     --retry 10 --retry-max-time 30 --retry-all-errors \
+     -X PUT \
+     -H "Content-type: application/json" \
+     -d '{ "name": "admin", "password": "admin" }' \
+     'http://localhost:4873/-/user/org.couchdb.user:admin' | jq .token)
+
+cat <<EOF > $packageDir/.npmrc
+; .npmrc
+enable-pre-post-scripts=true
+//localhost:4873/:_authToken=$token
+@fireproof:registry=http://localhost:4873
+EOF
+(cd $packageDir && pnpm publish --no-git-checks)
 
 tmpDir=$(mktemp -d)
 rm -rf node_modules dist pnpm-lock.yaml
 cp -pr * $tmpDir
 cd $tmpDir
-cp $projectRoot/dist/npmrc-smoke .npmrc
 cp package-template.json package.json
 pnpm install
-#pnpm add '@fireproof/core'
 pnpm run test
 rm -rf $tmpDir
+cd $smokeDir
+$dockerCompose down
