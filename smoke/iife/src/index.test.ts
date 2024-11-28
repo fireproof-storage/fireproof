6dd2c1ba2c7352aff8eaaf18e3df9a792adaf7dc
Meno Abels <meno.abels@adviser.com>
2024-11-28 10:16:49 +0100

    chore: fix the test

--- smoke/iife/src/index.test.ts
+++ smoke/iife/src/index.test.ts
@@ -1,32 +1,27 @@
 import { page } from "@vitest/browser/context";
 import { expect, it, vi } from "vitest";
 
+
 it("iife", async () => {
-  await page.goto("index.html");
-  await page.waitForLoadState("domcontentloaded");
+  let script = document.createElement("script");
+  script.textContent = `
+import { fireproof } from 'http://localhost:4874/@fireproof/core'
 
-  // const ready = page.locator("[data-ready]");
-  // await ready.waitFor();
+async function main() {
+  const db = fireproof("iife-test");
+  const ok = await db.put({ sort: Math.random(), test: "iife-success" });
 
-  // document.body.innerHTML = `<label data-testid="label" id="label"></label>`;
-
-  // console.log(document.querySelector("html").innerHTML);
-  // console.log(document.readyState);
-
-  // console.log(document.location.href);
-
-  await vi.waitFor(() => {
-    return new Promise((resolve) => {
-      if (document.readyState === "complete") resolve();
-      document.addEventListener("DOMContentLoaded", resolve);
-    });
-  }, 15_000);
-
-  console.log("After:", document.body.innerHTML);
+  const res = await db.get(ok.id)
+  const label = document.querySelector('label')
+  label.innerHTML = res.test
+  label.setAttribute("data-ready", "");
+}
+main().catch(console.error)
+`
+  script.type="module"
+  document.body.innerHTML=`<label data-testid="label" id="label"></label>`
+  document.body.appendChild(script);
 
   const el = await vi.waitUntil(() => document.querySelector("[data-ready]"), { timeout: 500_000 });
-
-  console.log("🪴", el);
-
   expect(await page.getByTestId("label").element().innerText).toBe("iife-success");
 });
