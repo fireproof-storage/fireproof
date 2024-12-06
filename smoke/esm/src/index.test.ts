a671495cce6c5712713a3fba330d9bec9f724dec
Meno Abels <meno.abels@adviser.com>
2024-12-06 06:16:34 +0100

    chore: improve test with esm.sh

--- smoke/esm/src/index.test.ts
+++ smoke/esm/src/index.test.ts
@@ -1,18 +1,39 @@
 import { page } from "@vitest/browser/context";
 import { expect, it, vi } from "vitest";
 
-it("iife", async () => {
+it("esm.sh", async () => {
   const script = document.createElement("script");
   script.textContent = `
 import { fireproof } from 'http://localhost:4874/@fireproof/core'
 
-async function main() {
-  const db = fireproof("iife-test");
-  const ok = await db.put({ sort: Math.random(), test: "iife-success" });
+function invariant(cond, message) {
+  if (!cond) {
+    throw new Error(message)
+  }
+}
+async function action(run) {
+  const db = fireproof("esm-test");
+  const ok = await db.put({ sort: Math.random(), test: "esm-success" });
+
+  const beforeAll = await db.allDocs()
+  await db.put({ foo: 1 })
+  const afterAll = await db.allDocs()
+
+  invariant(
+     afterAll.rows.length == beforeAll.rows.length + 1,
+     "all docs wrong count",
+  )
 
   const res = await db.get(ok.id)
   const label = document.querySelector('label')
-  label.innerHTML = res.test
+  label.innerHTML = [run,res.test].join(' - ')
+  await db.close()
+}
+
+async function main() {
+  for (let i = 0; i < 10; i++) {
+    await action(i)
+  }
   label.setAttribute("data-ready", "");
 }
 main().catch(console.error)
@@ -22,5 +43,5 @@ main().catch(console.error)
   document.body.appendChild(script);
 
   await vi.waitUntil(() => document.querySelector("[data-ready]"), { timeout: 500_000 });
-  expect(await page.getByTestId("label").element().innerText).toBe("iife-success");
+  expect(await page.getByTestId("label").element().innerText).toBe("9 - esm-success");
 });
