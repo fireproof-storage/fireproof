56b141631890a627a1c35517565911aa35e7d339
Meno Abels <meno.abels@adviser.com>
2024-12-10 17:10:11 -0500

    chore: added pull to [skip ci]

--- build-docs.sh
+++ build-docs.sh
@@ -2,9 +2,9 @@
 GIT=$(git rev-parse HEAD)
 rm -rf ./dist/fireproof-docs
 git clone $(pwd)/.git -b docs ./dist/fireproof-docs
-(cd ./dist/fireproof-docs && git rm -rf docs)
+cp .git/config ./dist/fireproof-docs/.git/
+(cd ./dist/fireproof-docs && git pull origin docs && git rm -rf docs)
 mkdir -p dist/fireproof-docs/docs
 npx typedoc --out dist/fireproof-docs/docs  src/database.ts
-cp .git/config ./dist/fireproof-docs/.git/
-(cd ./dist/fireproof-docs && git add `find docs -print` && git status && git commit -am "build from ${GIT} [skip ci]" && git push origin docs)
+(cd ./dist/fireproof-docs && git add docs && git status && git commit -am "build from ${GIT} [skip ci]" && git push origin docs)
 
