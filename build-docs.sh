e757faff0fbfa81c34c0fdbdea14cbcbb36a6c59
Meno Abels <meno.abels@adviser.com>
2024-12-10 16:58:15 -0500

    chore: now push to remote

--- build-docs.sh
+++ build-docs.sh
@@ -5,5 +5,6 @@ git clone $(pwd)/.git -b docs ./dist/fireproof-docs
 (cd ./dist/fireproof-docs && git rm -rf docs)
 mkdir -p dist/fireproof-docs/docs
 npx typedoc --out dist/fireproof-docs/docs  src/database.ts
-(cd ./dist/fireproof-docs && git add `find docs -print` && git status && git commit -am "build from ${GIT}" && git push)
+cp .git/config ./dist/fireproof-docs/.git/
+(cd ./dist/fireproof-docs && git add `find docs -print` && git status && git commit -am "build from ${GIT}" && git push origin docs)
 
