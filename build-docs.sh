c011e8d1980e36c71b2a13feab86f3089ab96653
Meno Abels <meno.abels@adviser.com>
2024-12-23 08:13:49 +0100

    chore: fix CI failure of build-docs

--- build-docs.sh
+++ build-docs.sh
@@ -3,8 +3,20 @@ GIT=$(git rev-parse HEAD)
 rm -rf ./dist/fireproof-docs
 git clone $(pwd)/.git -b docs ./dist/fireproof-docs
 cp .git/config ./dist/fireproof-docs/.git/
-(cd ./dist/fireproof-docs && git pull origin docs && git rm -rf docs)
+(
+  cd ./dist/fireproof-docs &&
+  git config advice.addIgnoredFile false &&
+  git pull origin docs &&
+  git rm -rf docs
+)
 mkdir -p dist/fireproof-docs/docs
 npx typedoc --out dist/fireproof-docs/docs  src/database.ts
-(cd ./dist/fireproof-docs && git add docs && git status && git commit -am "build from ${GIT} [skip ci]" && git push origin docs)
+
+cd ./dist/fireproof-docs
+git add docs
+if $(git status --porcelain)
+then
+  git commit -am "build from ${GIT} [skip ci]"
+  git push origin docs
+fi
 
