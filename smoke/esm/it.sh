c56203f116ba8bf35d601350cc1bd26c89e9bd8c
Meno Abels <meno.abels@adviser.com>
2025-01-10 17:04:10 +0100

    chore: smoke tests

--- smoke/esm/it.sh
+++ smoke/esm/it.sh
@@ -4,49 +4,53 @@ projectBase=$(pwd)
 cd smoke/esm
 smokeDir=$(pwd)
 
-if which docker-compose
-then
-  dockerCompose="docker-compose"
-else
-  dockerCompose="docker compose"
-fi
-cat > .env <<EOF
-PROJECT_BASE=$projectBase
-EOF
-mkdir -p $projectBase/.esm-cache/vd $projectBase/.esm-cache/esm
-chmod -R oug+w $projectBase/.esm-cache/vd $projectBase/.esm-cache/esm
-ls -la $projectBase/.esm-cache
-$dockerCompose down || true
-$dockerCompose up -d
+#if which docker-compose
+#then
+#  dockerCompose="docker-compose"
+#else
+#  dockerCompose="docker compose"
+#fi
+
+#cat > .env <<EOF
+#PROJECT_BASE=$projectBase
+#EOF
+#mkdir -p $projectBase/.esm-cache/vd $projectBase/.esm-cache/esm
+#chmod -R oug+w $projectBase/.esm-cache/vd $projectBase/.esm-cache/esm
+#ls -la $projectBase/.esm-cache
+#$dockerCompose down || true
+#$dockerCompose up -d
 packageDir=${projectBase=}/dist/fireproof-core
 
-user="admin$(date +%s)"
-curl --retry 10 --retry-max-time 30 --retry-all-errors http://localhost:4873/
-curl \
-     -X PUT \
-     -H "Content-type: application/json" \
-     -d "{ \"name\": \"$user\", \"password\": \"admin\" }" \
-     'http://localhost:4873/-/user/org.couchdb.user:$user'
+#user="admin$(date +%s)"
+#curl --retry 10 --retry-max-time 30 --retry-all-errors http://localhost:4873/
+#curl \
+#     -X PUT \
+#     -H "Content-type: application/json" \
+#     -d "{ \"name\": \"$user\", \"password\": \"admin\" }" \
+#     'http://localhost:4873/-/user/org.couchdb.user:$user'
+#
+#user="admin$(date +%s)"
+#token=$(curl \
+#     -X PUT \
+#     -H "Content-type: application/json" \
+#     -d "{ \"name\": \"$user\", \"password\": \"admin\" }" \
+#     'http://localhost:4873/-/user/org.couchdb.user:$user' | jq .token)
+#
+#echo "Token: $user:$token"
+#cat <<EOF > $packageDir/.npmrc
+#; .npmrc
+#enable-pre-post-scripts=true
+#//localhost:4873/:_authToken=$token
+#@fireproof:registry=http://localhost:4873
+#EOF
 
-user="admin$(date +%s)"
-token=$(curl \
-     -X PUT \
-     -H "Content-type: application/json" \
-     -d "{ \"name\": \"$user\", \"password\": \"admin\" }" \
-     'http://localhost:4873/-/user/org.couchdb.user:$user' | jq .token)
-
-echo "Token: $user:$token"
-cat <<EOF > $packageDir/.npmrc
-; .npmrc
-enable-pre-post-scripts=true
-//localhost:4873/:_authToken=$token
-@fireproof:registry=http://localhost:4873
-EOF
-(cd $packageDir &&
-	(npm unpublish --force || true) &&
-         npm publish --no-git-checks)
+#(cd $packageDir &&
+#	(npm unpublish --force || true) &&
+#         npm publish --no-git-checks)
 
 tmpDir=$(mktemp -d)
+cp $projectBase/dist/npmrc-smoke .npmrc
+unset npm_config_registry
 rm -rf node_modules dist pnpm-lock.yaml
 cp -pr * $tmpDir
 cd $tmpDir
@@ -55,4 +59,4 @@ pnpm install
 pnpm run test
 rm -rf $tmpDir
 cd $smokeDir
-$dockerCompose down
+#$dockerCompose down
