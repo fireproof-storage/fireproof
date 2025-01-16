ba6c07c6310aba4333ec252191f75d32efd429ff
Meno Abels <meno.abels@adviser.com>
2025-01-16 19:23:02 +0100

    chore: added VITE_API_URL

--- dashboard/src/helpers.ts
+++ dashboard/src/helpers.ts
@@ -6,4 +6,4 @@ export function truncateDbName(name: string, maxLength: number) {
 export const DEFAULT_ENDPOINT = "fireproof://cloud.fireproof.direct?getBaseUrl=https://storage.fireproof.direct/";
 export const SYNC_DB_NAME = "fp_sync";
 
-export const API_URL = "/api";
+export const API_URL = import.meta.env.VITE_API_URL ?? "/api";
