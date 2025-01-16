d0f2eb6e4ba6641dbdd9a96fe4ab69c34559f574
Meno Abels <meno.abels@adviser.com>
2025-01-16 18:33:39 +0100

    chore: added CloudContext to reduce the API calls

--- dashboard/src/pages/cloud/tenant-show.tsx
+++ dashboard/src/pages/cloud/tenant-show.tsx
@@ -1,9 +1,12 @@
 import { useParams } from "react-router-dom";
 import { tenantName, useListTendantsByUser } from "../../hooks/tenant.ts";
+import { useContext } from "react";
+import { CloudContext } from "../cloud.tsx";
 
 export function TenantShow() {
   const { tenantId } = useParams();
-  const list = useListTendantsByUser();
+  const { getListTenants } = useContext(CloudContext);
+  const list = getListTenants();
   const tenant = list.tenants.find((t) => t.tenantId === tenantId);
   if (!tenant) {
     return <>Tenant not{tenantId} found</>;
