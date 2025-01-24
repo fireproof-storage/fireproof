2106eb0ce4e27f281be1ad3c4fb66bea0f535143
Meno Abels <meno.abels@adviser.com>
2025-01-24 15:22:00 +0100

    chore: fix CI smoke

--- smoke/publish-local-registry.sh
+++ smoke/publish-local-registry.sh
@@ -56,14 +56,15 @@ echo $FP_VERSION > $projectRoot/dist/fp-version
 for packageDir in $projectRoot/dist/use-fireproof $projectRoot/dist/fireproof-core
 do
   smokeDir=$projectRoot/dist/smoke/$(basename $packageDir)
+  rm -rf $smokeDir
   mkdir -p $smokeDir
   cp -pr $packageDir/ $smokeDir/
-	cp $projectRoot/dist/npmrc-smoke $smokeDir/.npmrc
-	(cd $smokeDir &&
-         pnpm version $(cat $projectRoot/dist/fp-version) --no-git-tag-version &&
-         cat .npmrc &&
-         pnpm publish --registry=http://localhost:4873 --no-git-checks)
+  cp $projectRoot/dist/npmrc-smoke $smokeDir/.npmrc
+  (cd $smokeDir &&
+     pnpm version $(cat $projectRoot/dist/fp-version) --no-git-tag-version &&
+     cat .npmrc &&
+     pnpm publish --registry=http://localhost:4873 --no-git-checks)
 done
 
-curl -L "http://localhost:4874/@fireproof/core@$(cat $projectRoot/dist/fp-version) &
+curl -L "http://localhost:4874/@fireproof/core@$(cat $projectRoot/dist/fp-version)" &
 
