a2255cf863a21fa141b493592d22578cdc1e57ad
J Chris Anderson <jchris@fireproof.storage>
2025-01-27 13:32:29 -0800

    format for tests

--- smoke/get-fp-version.js
+++ smoke/get-fp-version.js
@@ -1,5 +1,5 @@
-import { $ } from 'zx'
-import * as process from 'node:process'
+import { $ } from "zx";
+import * as process from "node:process";
 
 function getVersion(version = "refs/tags/v0.0.0-smoke") {
   if (process.env.GITHUB_REF && process.env.GITHUB_REF.startsWith("refs/tags/v")) {
@@ -9,14 +9,14 @@ function getVersion(version = "refs/tags/v0.0.0-smoke") {
 }
 
 async function main() {
-  const gitHead = (await $`git rev-parse --short HEAD`).stdout.trim()
-  const dateTick = (await $`date +%s`).stdout.trim()
+  const gitHead = (await $`git rev-parse --short HEAD`).stdout.trim();
+  const dateTick = (await $`date +%s`).stdout.trim();
   // eslint-disable-next-line no-console, no-undef
-  console.log(getVersion(`refs/tags/v0.0.0-smoke-${gitHead}-${dateTick}`))
+  console.log(getVersion(`refs/tags/v0.0.0-smoke-${gitHead}-${dateTick}`));
 }
 
 main().catch((e) => {
   // eslint-disable-next-line no-console, no-undef
-	console.error(e)
-	process.exit(1)
-})
+  console.error(e);
+  process.exit(1);
+});
