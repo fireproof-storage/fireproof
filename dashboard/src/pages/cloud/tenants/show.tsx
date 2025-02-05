1d4efa28c5f33f8b587cc894fb1605af0feb3c03
Selem Delul <selem@fireproof.storage>
2025-02-05 16:24:14 -0500

    Extract url driven tabbed nav to its own component

--- dashboard/src/pages/cloud/tenants/show.tsx
+++ dashboard/src/pages/cloud/tenants/show.tsx
@@ -1,7 +1,8 @@
 import { useContext } from "react";
-import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
-import { UserTenant } from "../../../../backend/api.ts";
+import { Outlet, useParams } from "react-router-dom";
+import type { UserTenant } from "../../../../backend/api.ts";
 import { AppContext } from "../../../app-context.tsx";
+import { TabNavigation } from "../../../components/TabNavigation.tsx";
 
 function isAdmin(ut: UserTenant) {
   return ut.role === "admin";
@@ -17,8 +18,6 @@ export function CloudTenantShow() {
   const { tenantId } = useParams();
   const { cloud } = useContext(AppContext);
   const listTenants = cloud.getListTenantsByUser();
-  const navigate = useNavigate();
-  const location = useLocation();
 
   if (listTenants.isPending) {
     return <div>Loading...</div>;
@@ -41,23 +40,7 @@ export function CloudTenantShow() {
   return (
     <div className="flex h-full">
       <div className="flex-1 overflow-auto">
-        <div className="flex border-b border-fp-dec-00 text-fp-p text-14 mb-4">
-          <nav className="flex" aria-label="Tabs">
-            {tabs.map((tab) => (
-              <button
-                key={tab.id}
-                onClick={() => navigate(tab.id)}
-                className={`
-                  px-4 py-2 border-b-2 select-none hover:border-fp-a-03
-                  ${location.pathname.endsWith(tab.id) ? "border-fp-a-03 text-fp-a-03" : "border-transparent"}
-                `}
-              >
-                {tab.label}
-              </button>
-            ))}
-          </nav>
-        </div>
-
+        <TabNavigation tabs={tabs} className="mb-4" />
         <div className="">
           <Outlet />
         </div>
