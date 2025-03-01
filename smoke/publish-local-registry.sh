18264bfdd86bec903b789d3db87527723480311a
J Chris Anderson <jchris@fireproof.storage>
2025-03-01 08:54:27 -0800

    try full dist tag

--- smoke/publish-local-registry.sh
+++ smoke/publish-local-registry.sh
@@ -62,7 +62,8 @@ do
      node $projectRoot/smoke/patch-fp-version.js package.json $(cat $projectRoot/dist/fp-version)
      cat .npmrc &&
      cat package.json &&
-     pnpm publish --registry=http://localhost:4873 --no-git-checks --tag smoke)
+     pnpm publish --registry=http://localhost:4873 --no-git-checks --tag smoke &&
+     npm dist-tag add $(node -e "console.log(require('./package.json').name)")@$(cat $projectRoot/dist/fp-version) latest --registry=http://localhost:4873)
 done
 
 curl -L "http://localhost:4874/@fireproof/core@$(cat $projectRoot/dist/fp-version)" > /dev/null &
