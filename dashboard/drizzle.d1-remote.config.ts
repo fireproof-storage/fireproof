e7216a566378fc4d2add2a4ae6e8df6b4a84a67b
Meno Abels <meno.abels@adviser.com>
2025-03-10 11:15:27 +0100

    chore: try d1 deploymen

--- dashboard/drizzle.d1-remote.config.ts
+++ dashboard/drizzle.d1-remote.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
   dbCredentials: {
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
     databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-    token: process.env.CLOUDFLARE_D1_TOKEN!,
+    token: process.env.CLOUDFLARE_API_TOKEN! ?? process.env.CLOUDFLARE_D1_TOKEN!,
   },
 });
