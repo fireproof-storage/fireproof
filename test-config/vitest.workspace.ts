2b552f64fb30356ab53b196d3a9265ebad51703f
J Chris Anderson <jchris@fireproof.storage>
2025-03-11 07:11:23 -0700

    remove test move

--- test-config/vitest.workspace.ts
+++
@@ -1,7 +0,0 @@
-import { defineWorkspace } from "vitest/config";
-
-export default defineWorkspace([
-  "./test-config/vitest.memory.config.ts",
-  "./test-config/vitest.file.config.ts",
-  "./test-config/vitest.indexeddb.config.ts",
-]);
