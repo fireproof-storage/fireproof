a1ce9a14d71308f10955872f7461269f078c917c
J Chris Anderson <jchris@fireproof.storage>
2025-03-11 07:11:23 -0700

    remove test move

--- test-config/setup.file.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line no-undef
-process.env.FP_STORAGE_URL = "./dist/fp-dir-file";
--- test-config/setup.indexeddb.ts
+++
@@ -1,8 +0,0 @@
-function gthis() {
-  return globalThis;
-}
-
-gthis()[Symbol.for("FP_PRESET_ENV")] = {
-  // FP_KEYBAG_URL = "memory://keybag"
-  // FP_DEBUG: "metaStoreFactory"
-};
