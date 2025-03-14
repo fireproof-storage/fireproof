150dd8ea7aad113d9d92b0433020cda7fb529cb6
Selem Delul <selem@fireproof.storage>
2025-03-13 21:26:22 -0400

    Don't redirect to a new login page on signin button click.

--- dashboard/src/pages/login.tsx
+++ dashboard/src/pages/login.tsx
@@ -1,7 +1,6 @@
-import { SignInButton } from "@clerk/clerk-react";
+import { SignIn } from "@clerk/clerk-react";
 import { useContext, useState } from "react";
 import { AppContext } from "../app-context.tsx";
-import { Button } from "../components/Button.tsx";
 const slides = [
   { text: "This is going to be the way to\u00A0make apps.", author: "Boorad / Brad Anderson", role: "startup founder" },
   { text: "Fastest I’ve ever developed any app of any kind.", author: "Mykle Hansen", role: "developer" },
@@ -110,17 +109,14 @@ export function Login() {
               Yes, I'd like to receive (occasional, genuinely informative) emails from Fireproof.
             </label>
           </div>
-          <SignInButton forceRedirectUrl="/fp/cloud">
-            <Button variation="tertiary" style="w-full" onClick={toggleDarkMode}>
-              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill="currentColor"
-                  d="M10 0C8.68678 0 7.38642 0.265079 6.17317 0.780103C4.95991 1.29513 3.85752 2.05001 2.92893 3.00165C1.05357 4.92357 0 7.53026 0 10.2483C0 14.778 2.87 18.6211 6.84 19.9841C7.34 20.0661 7.5 19.7484 7.5 19.4717V17.7398C4.73 18.3547 4.14 16.3665 4.14 16.3665C3.68 15.1777 3.03 14.86 3.03 14.86C2.12 14.2246 3.1 14.2451 3.1 14.2451C4.1 14.3168 4.63 15.3007 4.63 15.3007C5.5 16.8584 6.97 16.3972 7.54 16.1513C7.63 15.4851 7.89 15.0342 8.17 14.778C5.95 14.5218 3.62 13.6404 3.62 9.73586C3.62 8.5983 4 7.6862 4.65 6.95857C4.55 6.70237 4.2 5.63655 4.75 4.25303C4.75 4.25303 5.59 3.97633 7.5 5.29836C8.29 5.07289 9.15 4.96016 10 4.96016C10.85 4.96016 11.71 5.07289 12.5 5.29836C14.41 3.97633 15.25 4.25303 15.25 4.25303C15.8 5.63655 15.45 6.70237 15.35 6.95857C16 7.6862 16.38 8.5983 16.38 9.73586C16.38 13.6507 14.04 14.5115 11.81 14.7678C12.17 15.0855 12.5 15.7106 12.5 16.6637V19.4717C12.5 19.7484 12.66 20.0764 13.17 19.9841C17.14 18.6109 20 14.778 20 10.2483C20 8.90245 19.7413 7.5698 19.2388 6.32643C18.7362 5.08305 17.9997 3.95329 17.0711 3.00165C16.1425 2.05001 15.0401 1.29513 13.8268 0.780103C12.6136 0.265079 11.3132 0 10 0Z"
-                />
-              </svg>
-              Continue with GitHub
-            </Button>
-          </SignInButton>
+          <SignIn
+            appearance={{
+              elements: {
+                headerSubtitle: { display: "none" },
+                footer: { display: "none" },
+              },
+            }}
+          />
 
           <svg
             className="absolute scale-[0.7] sm:scale-100 right-[-68px] bottom-[60px] sm:right-[-60px] sm:bottom-[65px] text-fp-a-02 pointer-events-none"
