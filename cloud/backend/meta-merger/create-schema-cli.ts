3d7d358dc4a7da6185a7bd1d9fd2e5c47f46e00e
Meno Abels <meno.abels@adviser.com>
2025-03-21 08:07:05 +0100

    chore: added drizzle+zod

--- cloud/backend/meta-merger/create-schema-cli.ts
+++
@@ -1,9 +0,0 @@
-import { MetaSendSql } from "./meta-send.js";
-
-async function main() {
-  // eslint-disable-next-line no-console
-  console.log(MetaSendSql.schema(true).join(";\n"));
-}
-
-// eslint-disable-next-line no-console
-main().catch(console.error);
