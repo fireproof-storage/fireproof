6b498cd5f034f4ad7ca25562513db1ffdd638515
J Chris Anderson <jchris@fireproof.storage>
2025-03-24 08:44:52 -0700

    refactor: remove deprecated hooks and clean up exports
    - Removed deprecated top level hooks: useDocument, useLiveQuery, useAllDocs, and useChanges from the react module.
    - Updated index.ts to reflect the removal of these hooks and cleaned up exports accordingly.
    - Enhanced documentation in useFireproof.ts to clarify usage of custom IDs and API methods for document management.

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,23 +1,3 @@
-export {
-  type TLUseDocument,
-  /** @deprecated Use return values from useFireproof('dbname') instead. Top level usage will be removed in future versions. */
-  useDocument,
-} from "./useDocument.js";
 export { FireproofCtx, type UseFireproof, useFireproof, type LiveQueryResult, type UseDocumentResult } from "./useFireproof.js";
-export {
-  type TLUseLiveQuery,
-  /** @deprecated Use return values from useFireproof('dbname') instead. Top level usage will be removed in future versions. */
-  useLiveQuery,
-} from "./useLiveQuery.js";
-export {
-  type TLUseAllDocs,
-  /** @deprecated Use return values from useFireproof('dbname') instead. Top level usage will be removed in future versions. */
-  useAllDocs,
-} from "./useAllDocs.js";
-export {
-  type TLUseChanges,
-  /** @deprecated Use return values from useFireproof('dbname') instead. Top level usage will be removed in future versions. */
-  useChanges,
-} from "./useChanges.js";
 
 export * from "./img-file.js";
