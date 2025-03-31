39c00a72872b2dbb9124f35c41afbba150c83a6d
Meno Abels <meno.abels@adviser.com>
2025-03-31 09:50:53 +0200

    add vite.config

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import react from "@vitejs/plugin-react";
+import * as path from "node:path";
+import { defineConfig } from "vite";
+// import { visualizer } from 'rollup-plugin-visualizer';
+
+// import { cloudflare } from "@cloudflare/vite-plugin";
+
+
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  server: {
+    port: 3000,
+    hmr: false,
+  },
+  build: {
+    sourcemap: true,
+    target: "esnext",
+    outDir: "./dist/static",
+    emptyOutDir: true, // also necessary
+  },
+  resolve: process.env.USE_SOURCE
+    ? {
+        alias: {
+          "react-router": path.resolve(__dirname, "../../packages/react-router/index.ts"),
+          "react-router-dom": path.resolve(__dirname, "../../packages/react-router-dom/index.tsx"),
+        },
+      }
+    : {},
+});
