24c4878fe04bb5248a06b0662ce3909afd5d8614
J Chris Anderson <jchris@fireproof.storage>
2025-04-07 12:45:17 -0700

    chore: rm notes, back to main on smoke

--- smoke/react/src/routes/Todo.tsx
+++ smoke/react/src/routes/Todo.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-// In the smoke tests, use-fireproof is available as a package name
-// This works because of how the smoke test environment is set up
 import { useFireproof } from "use-fireproof";
 
 type Todo = Partial<{
@@ -10,107 +8,121 @@ type Todo = Partial<{
 }>;
 
 export default function TodoList() {
-  const { database, useDocument, useLiveQuery } = useFireproof("TodoDB");
-  const [editingTodoId, setEditingTodoId] = useState<string | null>(null);
+  const { useDocument, useLiveQuery } = useFireproof("TodoDB");
+  const [selectedTodo, setSelectedTodo] = useState<string>("");
   const todos = useLiveQuery<Todo>("date", { limit: 1000, descending: true });
-  const [newTodo, setNewTodo, saveNewTodo] = useDocument<Todo>({
+  const [todo, setTodo, saveTodo] = useDocument<Todo>({
     text: "",
     date: Date.now(),
     completed: false,
   });
-
-  // Handle the editing of existing todos
-  const { doc: editingTodo, merge, submit } = useDocument<Todo>(editingTodoId ? { _id: editingTodoId } : null);
-
-  const handleToggleComplete = async (todo: Todo & { _id: string }) => {
-    await database.put({
-      ...todo,
-      completed: !todo.completed,
-    });
-  };
-
-  const handleAddTodo = async () => {
-    if (!newTodo.text) return;
-
-    try {
-      await saveNewTodo();
-      setNewTodo({ text: "", date: Date.now(), completed: false });
-    } catch (e) {
-      console.error("Add-Todo Error:", e);
-    }
-  };
-
-  const handleEditClick = (todoId: string) => {
-    setEditingTodoId(todoId);
-  };
-
-  const handleCancelEdit = () => {
-    setEditingTodoId(null);
-  };
-
-  const handleSaveEdit = async () => {
-    await submit();
-    setEditingTodoId(null);
-  };
-
-  const handleDeleteTodo = async (todoId: string) => {
-    await database.remove(todoId);
-    if (editingTodoId === todoId) {
-      setEditingTodoId(null);
-    }
-  };
+  // console.log("todos", todo, todos.docs.length) //todos.docs.map((t) => t.text));
 
   return (
     <>
-      <div className="todo-form">
+      <div>
         <input
           type="text"
-          value={newTodo.text || ""}
-          placeholder="Add a new todo"
-          onChange={(e) => setNewTodo({ text: e.target.value.trim() })}
-          onKeyDown={(e) => e.key === "Enter" && handleAddTodo()}
+          value={todo.text}
+          placeholder="new todo here"
+          onChange={(e) => {
+            // console.log("setting todo", e.target.value);
+            setTodo({ text: e.target.value.trim() });
+          }}
         />
-        <button onClick={handleAddTodo}>Add Todo</button>
+        <button
+          onClick={() => {
+            // console.log(`saving todo-0: ${todo.text}`);
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            delete (todo as any)._id;
+            // console.log(`saving todo-1: ${todo.text}`);
+            // console.log("saving todo", todo, saveTodo.toString());
+            saveTodo()
+              .then(() => {
+                // console.log(`saving todo-2: ${todo.text}`);
+                // console.log("saved todo", todo.text);
+                setTodo({ text: "", _id: undefined });
+              })
+              .catch((e) => console.error("Add-Todo Error:", e));
+          }}
+        >
+          Add Todo
+        </button>
       </div>
-
-      <div className="todo-list">
-        {todos.docs.map((todo: Todo & { _id: string }) => (
-          <div key={todo._id} className="todo-item">
-            {editingTodoId === todo._id ? (
-              // Edit mode
-              <div className="todo-edit-form">
-                <input type="text" value={editingTodo.text || ""} onChange={(e) => merge({ text: e.target.value.trim() })} />
-                <input
-                  type="checkbox"
-                  checked={editingTodo.completed || false}
-                  onChange={() => merge({ completed: !editingTodo.completed })}
-                />
-                <button onClick={handleSaveEdit}>Save</button>
-                <button onClick={handleCancelEdit}>Cancel</button>
-                <button onClick={() => handleDeleteTodo(todo._id as string)}>Delete</button>
-              </div>
-            ) : (
-              // View mode
-              <>
-                <input
-                  type="checkbox"
-                  checked={todo.completed || false}
-                  onChange={() => handleToggleComplete(todo as Todo & { _id: string })}
-                />
-                <span
-                  style={{
-                    textDecoration: todo.completed ? "line-through" : "none",
-                    cursor: "pointer",
-                  }}
-                  onClick={() => handleEditClick(todo._id as string)}
-                >
-                  {todo.text}
-                </span>
-              </>
-            )}
-          </div>
-        ))}
-      </div>
+      {todos.docs.map((todo) => (
+        <div key={todo._id}>
+          <input
+            type="radio"
+            checked={selectedTodo === todo._id}
+            onChange={() => {
+              // console.log("selected todo", todo._id);
+              setSelectedTodo(todo._id as string);
+            }}
+          />
+          <span
+            style={{
+              textDecoration: todo.completed ? "line-through" : "none",
+            }}
+          >
+            {todo.text}
+          </span>
+        </div>
+      ))}
+      {selectedTodo && <TodoEditor id={selectedTodo} />}
     </>
+  );
+}
+
+interface TodoEditorProps {
+  readonly id: string;
+}
+
+function TodoEditor({ id }: TodoEditorProps) {
+  const { useDocument } = useFireproof("TodoDB");
+  const [todo, setTodo, saveTodo, deleteTodo] = useDocument<Todo>({
+    _id: id, // showcase modifying an existing document
+    text: "",
+    date: Date.now(),
+    completed: false,
+  });
+  // console.log("editing todo", todo);
+
+  return (
+    <div id="todo-editor">
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={async () => {
+          // console.log("toggling todo", todo);
+          await saveTodo({ ...todo, completed: !todo.completed });
+          // console.log("toggling todo", todo, res);
+          setTodo();
+        }}
+      />
+      <input
+        type="text"
+        value={todo.text}
+        placeholder="update todo here"
+        onChange={(e) => {
+          setTodo({ text: e.target.value.trim() });
+        }}
+      />
+      <button
+        onClick={async () => {
+          await saveTodo();
+          setTodo();
+        }}
+      >
+        Save Changes
+      </button>
+      <button
+        onClick={async () => {
+          await deleteTodo();
+          setTodo();
+        }}
+      >
+        Delete
+      </button>
+    </div>
   );
 }
