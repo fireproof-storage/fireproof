ed5beebb02cd89c177eeadf01b7b2e8a258c8b11
Chris Anderson <jchris@fireproof.storage>
2025-04-07 10:55:06 -0700

    Jchris/cleanup (#700)
    * remove scripts unused files
    
    * remove scripts unused files
    
    * remove old examples, link to new
    
    * fix: clean up README formatting and remove unnecessary whitespace

--- scripts/react/src/index.tsx
+++
@@ -1,4 +0,0 @@
-export { type TLUseDocument, useDocument } from "./useDocument";
-export { FireproofCtx, type UseFireproof, useFireproof } from "./useFireproof";
-export { type TLUseLiveQuery, useLiveQuery } from "./useLiveQuery";
-export * from "@fireproof/core";
