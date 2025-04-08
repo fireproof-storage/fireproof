c2e53e97c564306271be5c03e21487846eedddb2
J Chris Anderson <jchris@fireproof.storage>
2025-04-07 18:50:03 -0700

    livequery changes

--- src/react/use-live-query.ts
+++ src/react/use-live-query.ts
@@ -2,6 +2,10 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import type { DocFragment, DocTypes, DocWithId, IndexKeyType, IndexRow, MapFn, Database } from "@fireproof/core";
 import type { LiveQueryResult } from "./types.js";
 
+// Internal shadow type for array-like behavior
+type ArrayLikeQueryResult<T extends DocTypes, K extends IndexKeyType, R extends DocFragment = T> = LiveQueryResult<T, K, R> &
+  DocWithId<T>[];
+
 /**
  * Implementation of the useLiveQuery hook
  */
@@ -11,17 +15,20 @@ export function createUseLiveQuery(database: Database) {
     query = {},
     initialRows: IndexRow<K, T, R>[] = [],
   ): LiveQueryResult<T, K, R> {
-    const [result, setResult] = useState<LiveQueryResult<T, K, R>>(() => {
+    const [result, setResult] = useState<ArrayLikeQueryResult<T, K, R>>(() => {
       const docs = initialRows.map((r) => r.doc).filter((r): r is DocWithId<T> => !!r);
-      return {
-        rows: initialRows,
+      return Object.assign(
+        {
+          docs,
+          rows: initialRows,
+          length: docs.length,
+          [Symbol.iterator]: () => docs[Symbol.iterator](),
+          map: <U>(fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => U) => docs.map(fn),
+          filter: (fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => boolean) => docs.filter(fn),
+          forEach: (fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => void) => docs.forEach(fn),
+        },
         docs,
-        length: docs.length,
-        map: <U>(fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => U) => docs.map(fn),
-        filter: (fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => boolean) => docs.filter(fn),
-        forEach: (fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => void) => docs.forEach(fn),
-        [Symbol.iterator]: () => docs[Symbol.iterator](),
-      } as LiveQueryResult<T, K, R>;
+      );
     });
 
     const queryString = useMemo(() => JSON.stringify(query), [query]);
@@ -29,22 +36,30 @@ export function createUseLiveQuery(database: Database) {
 
     const refreshRows = useCallback(async () => {
       const res = await database.query<K, T, R>(mapFn, query);
-      const docs = res.rows.map((r) => r.doc as DocWithId<T>).filter((r): r is DocWithId<T> => !!r);
-      setResult({
-        ...res,
-        docs,
-        length: docs.length,
-        map: <U>(fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => U) => docs.map(fn),
-        filter: (fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => boolean) => docs.filter(fn),
-        forEach: (fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => void) => docs.forEach(fn),
-        [Symbol.iterator]: () => docs[Symbol.iterator](),
-      } as LiveQueryResult<T, K, R>);
-    }, [mapFnString, queryString]);
+      const docs = res.rows.map((r) => r.doc).filter((r): r is DocWithId<T> => !!r);
+      setResult(
+        Object.assign(
+          {
+            docs,
+            rows: res.rows,
+            length: docs.length,
+            [Symbol.iterator]: () => docs[Symbol.iterator](),
+            map: <U>(fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => U) => docs.map(fn),
+            filter: (fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => boolean) => docs.filter(fn),
+            forEach: (fn: (value: DocWithId<T>, index: number, array: DocWithId<T>[]) => void) => docs.forEach(fn),
+          },
+          docs,
+        ),
+      );
+    }, [database, mapFnString, queryString]);
 
     useEffect(() => {
-      refreshRows(); // Initial data fetch
-      return database.subscribe(refreshRows);
-    }, [refreshRows]);
+      refreshRows();
+      const unsubscribe = database.subscribe(refreshRows);
+      return () => {
+        unsubscribe();
+      };
+    }, [database, refreshRows]);
 
     return result;
   };
