db41f4723b01808ee337b70efebab30f24758c15
Meno Abels <meno.abels@adviser.com>
2025-06-09 10:15:54 +0200

    chore: fix pnpm-lock

--- dashboard/vitest.config.ts
+++ dashboard/vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vitest/config";
 
+import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     globals: true,
     globalSetup: "./globalSetup.libsql.ts",
