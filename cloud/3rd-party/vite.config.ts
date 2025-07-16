9a37fb21a504ed7a4b141f33b7b7944e3ac328be
Meno Abels <meno.abels@adviser.com>
2025-07-16 21:39:33 +0200

    format:

--- cloud/3rd-party/vite.config.ts
+++ cloud/3rd-party/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   build: {
     sourcemap: true,
     target: "esnext",
