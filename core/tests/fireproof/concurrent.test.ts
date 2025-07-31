183b49e9086d52b6139e164f7251e60d016eeea7
Meno Abels <meno.abels@adviser.com>
2025-07-31 08:38:30 +0200

    chore: cleanup versions string to use AFL-2.0 chore: thx coderabbit

--- core/tests/fireproof/concurrent.test.ts
+++ core/tests/fireproof/concurrent.test.ts
@@ -13,39 +13,26 @@ describe("concurrent opens", () => {
     const loops = 100;
     for (let i = 0; i < loops; i++) {
       // eslint-disable-next-line no-async-promise-executor
-      setTimeout(async () => {
-        // for (let t = 0; t < 10; t++) {
-        //   const db = fireproof(`${dbBaseName}-${i}`);
-        //   await db.put({ _id: `test-${t}`, foo: "bar" });
-        //   await db.close();
-        // }
-        const db = fireproof(`${dbBaseName}-${i}`);
-        await Promise.all(
-          new Array(3).fill(0).map(async (_, j) => {
-            await db.put({ _id: `test-${j}`, foo: "bar" });
-          }),
-        );
-        const allDocs = await db.allDocs();
-        expect(allDocs.rows.length).toBe(3);
-        await db.close();
-        dones.push(i);
-        if (dones.length >= loops) {
-          future.resolve(undefined);
-        }
-      }, 0);
-      //await db.destroy();
+      setTimeout(
+        async () => {
+          const db = fireproof(`${dbBaseName}-${i}`);
+          await Promise.all(
+            new Array(3).fill(0).map(async (_, j) => {
+              await db.put({ _id: `test-${j}`, foo: "bar" });
+            }),
+          );
+          const allDocs = await db.allDocs();
+          expect(allDocs.rows.length).toBe(3);
+          await db.close();
+          dones.push(i);
+          if (dones.length >= loops) {
+            future.resolve(undefined);
+          }
+        },
+        ~~(Math.random() * 17),
+      );
     }
 
     await future.asPromise();
-    // await Promise.all(new Array(1000).fill(0).map(async (_, i) => {
-    //     const db = fireproof(`${dbBaseName}-${i}`);
-    //     await Promise.all(new Array(3).fill(0).map(async (_, j) => {
-    //         await db.put({ _id: `test-${j}`, foo: "bar" });
-    //     }));
-    //     const allDocs = await db.allDocs();
-    //     expect(allDocs.rows.length).toBe(3);
-    //     await db.close();
-    //     // await db.destroy();
-    // }));
   });
 });
