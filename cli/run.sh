1af0280796c45482ebd7eecd2675eb1af4b589c3
Meno Abels <meno.abels@adviser.com>
2025-08-05 09:04:28 +0200

    chore: set default to tsgo

--- cli/run.sh
+++ cli/run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#echo "run.sh $@,$1,$FP_TSC"
+#echo "run.sh $0,$1,$FP_TSC $PATH"
 if [ "$1" = "tsc" ]
 then
   if [ -z "$FP_TSC" ]
   then
     FP_TSC=tsgo
   fi
-  exec pnpm exec $FP_TSC $@
+  #echo $PWD
+  #which $FP_TSC
+  shift
+  #echo $(which $FP_TSC) $@
+  exec $(which $FP_TSC) $@
 fi
 
 exec pnpm exec tsx $(dirname $0)/main.ts $@
