e2617a395bf1872298927a6346b3160111e32565
Meno Abels <meno.abels@adviser.com>
2025-08-19 19:40:42 +0200

    chore: do not use the real env for version calculation tests

--- cli/build-cmd.test.ts
+++ cli/build-cmd.test.ts
@@ -2,100 +2,106 @@ import { getVersion } from "./build-cmd.js";
 import { expect, it } from "vitest";
 
 it("getVersion emptyString", async () => {
-  expect(await getVersion("")).toContain("0.0.0-smoke");
+  expect(await getVersion("", { xenv: {} })).toContain("0.0.0-smoke");
 });
 
 it("getVersion file with v", async () => {
   expect(
     await getVersion("wurst", {
-      existsSync: () => true,
-      readFile: (_x: string, _y = "utf-8") => Promise.resolve("v0.0.48-smoke"),
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } as any),
+      xfs: {
+        existsSync: () => true,
+        readFile: (_x: string, _y = "utf-8") => Promise.resolve("v0.0.48-smoke"),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any,
+      xenv: {},
+    }),
   ).toContain("0.0.48-smoke");
 });
 
 it("getVersion file without ", async () => {
   expect(
     await getVersion("wurst", {
-      existsSync: () => true,
-      readFile: (_x: string, _y = "utf-8") => Promise.resolve("0.0.48-smoke"),
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } as any),
+      xfs: {
+        existsSync: () => true,
+        readFile: (_x: string, _y = "utf-8") => Promise.resolve("0.0.48-smoke"),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any,
+      xenv: {},
+    }),
   ).toContain("0.0.48-smoke");
 });
 
 it("getVersion file with scope", async () => {
   expect(
     await getVersion("wurst", {
-      existsSync: () => true,
-      readFile: (_x: string, _y = "utf-8") => Promise.resolve("wost@0.0.48-smoke"),
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } as any),
+      xfs: {
+        existsSync: () => true,
+        readFile: (_x: string, _y = "utf-8") => Promise.resolve("wost@0.0.48-smoke"),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any,
+      xenv: {},
+    }),
   ).toContain("0.0.48-smoke");
 });
 
 it("getVersion file with scope and v", async () => {
   expect(
     await getVersion("wurst", {
-      existsSync: () => true,
-      readFile: (_x: string, _y = "utf-8") => Promise.resolve("wost@v0.0.48-smoke"),
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } as any),
+      xfs: {
+        existsSync: () => true,
+        readFile: (_x: string, _y = "utf-8") => Promise.resolve("wost@v0.0.48-smoke"),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any,
+      xenv: {},
+    }),
   ).toContain("0.0.48-smoke");
 });
 
 it("getVersion file with ref", async () => {
   expect(
     await getVersion("wurst", {
-      existsSync: () => true,
-      readFile: (_x: string, _y = "utf-8") => Promise.resolve("d/d/wost@0.0.48-smoke"),
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } as any),
+      xfs: {
+        existsSync: () => true,
+        readFile: (_x: string, _y = "utf-8") => Promise.resolve("d/d/wost@0.0.48-smoke"),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any,
+      xenv: {},
+    }),
   ).toContain("0.0.48-smoke");
 });
 
 it("getVersion with v", async () => {
-  process.env.GITHUB_REF = "v0.0.8-smoke";
-  expect(await getVersion()).toContain("0.0.8-smoke");
+  expect(await getVersion(undefined, { xenv: { GITHUB_REF: "v0.0.8-smoke" } })).toContain("0.0.8-smoke");
 });
 
 it("getVersion with scope and v", async () => {
-  process.env.GITHUB_REF = "blub@v0.0.8-smoke";
-  expect(await getVersion()).toContain("0.0.8-smoke");
+  expect(await getVersion(undefined, { xenv: { GITHUB_REF: "blub@v0.0.8-smoke" } })).toContain("0.0.8-smoke");
 });
 
 it("getVersion with scope and no v", async () => {
-  process.env.GITHUB_REF = "blub@0.0.8-smoke";
-  expect(await getVersion()).toContain("0.0.8-smoke");
+  expect(await getVersion(undefined, { xenv: { GITHUB_REF: "blub@0.0.8-smoke" } })).toContain("0.0.8-smoke");
 });
 
 it("getVersion without", async () => {
-  process.env.GITHUB_REF = "0.0.9-smoke";
-  expect(await getVersion()).toContain("0.0.9-smoke");
+  expect(await getVersion(undefined, { xenv: { GITHUB_REF: "0.0.9-smoke" } })).toContain("0.0.9-smoke");
 });
 
 it("getVersion ref without", async () => {
-  process.env.GITHUB_REF = "v0.0.9-smoke";
-  expect(await getVersion()).toContain("0.0.9-smoke");
+  expect(await getVersion(undefined, { xenv: { GITHUB_REF: "v0.0.9-smoke" } })).toContain("0.0.9-smoke");
 });
 
 it("getVersion ref with v", async () => {
-  process.env.GITHUB_REF = "a/ref/v0.0.8-smoke";
-  expect(await getVersion()).toContain("0.0.8-smoke");
+  expect(await getVersion(undefined, { xenv: { GITHUB_REF: "a/ref/v0.0.8-smoke" } })).toContain("0.0.8-smoke");
 });
 
 it("getVersion ref with scope and v", async () => {
-  process.env.GITHUB_REF = "a/ref/blub@v0.0.8-smoke";
-  expect(await getVersion()).toContain("0.0.8-smoke");
+  expect(await getVersion(undefined, { xenv: { GITHUB_REF: "a/ref/blub@v0.0.8-smoke" } })).toContain("0.0.8-smoke");
 });
 
 it("getVersion ref with scope and no v", async () => {
-  process.env.GITHUB_REF = "a/ref/blub@0.0.8-smoke";
-  expect(await getVersion()).toContain("0.0.8-smoke");
+  expect(await getVersion(undefined, { xenv: { GITHUB_REF: "a/ref/blub@0.0.8-smoke" } })).toContain("0.0.8-smoke");
 });
 
 it("getVersion ref without", async () => {
-  process.env.GITHUB_REF = "a/ref/0.0.9-smoke";
-  expect(await getVersion()).toContain("0.0.9-smoke");
+  expect(await getVersion(undefined, { xenv: { GITHUB_REF: "a/ref/0.0.9-smoke" } })).toContain("0.0.9-smoke");
 });
