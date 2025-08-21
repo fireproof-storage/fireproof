48e884dce056f3a32c6bd39c66c06328cf6b7271
J Chris Anderson <jchris@fireproof.storage>
2025-08-21 11:57:27 -0700

    fix: trigger subscriptions when remote data syncs in via CRDT clock updates

--- core/base/crdt-clock.ts
+++ core/base/crdt-clock.ts
@@ -115,6 +115,9 @@ export class CRDTClockImpl {
 
     const noLoader = !localUpdates;
 
+    // 🐛 FIX: Track if we need to manually trigger subscriptions for remote sync
+    const needsManualNotification = !localUpdates && this.watchers.size > 0;
+
     // console.log("int_applyHead", this.applyHeadQueue.size(), this.head, newHead, prevHead, localUpdates);
     const ogHead = sortClockHead(this.head);
     newHead = sortClockHead(newHead);
@@ -156,6 +159,17 @@ export class CRDTClockImpl {
       this.transaction = undefined;
     }
     this.setHead(advancedHead);
+
+    // 🐛 FIX: Manually trigger subscriptions for remote sync data
+    // This ensures that db.subscribe() callbacks fire when remote data syncs in,
+    // fixing the issue where React useLiveQuery doesn't update on remote changes
+    if (needsManualNotification) {
+      // Get the changes that were applied from remote sync
+      const changes = await clockChangesSince<DocTypes>(this.blockstore, advancedHead, prevHead, {}, this.logger);
+      if (changes.result.length > 0) {
+        this.notifyWatchers(changes.result);
+      }
+    }
   }
 }
 
