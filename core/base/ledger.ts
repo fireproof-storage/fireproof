417c8b5e495591f06e705fac680f6b0c242b1b6f
Meno Abels <meno.abels@adviser.com>
2025-09-19 12:20:48 +0200

    chore: now the stall is gone

--- core/base/ledger.ts
+++ core/base/ledger.ts
@@ -48,7 +48,7 @@ export function LedgerFactory(name: string, opts?: ConfigOpts): Ledger {
   const key = keyConfigOpts(sthis, name, opts);
   const item = ledgers.get(key);
   return new LedgerShell(
-    item.once((key) => {
+    item.once(({key}) => {
       const db = new LedgerImpl(sthis, {
         name,
         meta: opts?.meta,
@@ -70,7 +70,7 @@ export function LedgerFactory(name: string, opts?: ConfigOpts): Ledger {
           }),
       });
       db.onClosed(() => {
-        ledgers.unget(key as string);
+        ledgers.unget(key);
       });
       return db;
     }),
