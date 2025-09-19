df57fa960a34b3d1c3b66376a2b5617122338ed9
Meno Abels <meno.abels@adviser.com>
2025-09-19 10:57:18 +0200

    chore: @adviser/cement update

--- core/base/ledger.ts
+++ core/base/ledger.ts
@@ -47,13 +47,8 @@ export function LedgerFactory(name: string, opts?: ConfigOpts): Ledger {
   const sthis = ensureSuperThis(opts);
   const key = keyConfigOpts(sthis, name, opts);
   const item = ledgers.get(key);
-  // if (!item.ready) {
-  // console.log("LedgerFactory", key);
-  // }
   return new LedgerShell(
     item.once((key) => {
-      // console.log("once-LedgerFactory", key);
-
       const db = new LedgerImpl(sthis, {
         name,
         meta: opts?.meta,
